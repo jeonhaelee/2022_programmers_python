c931063d42628feb9d10573fcddc787811db1eb3
jeonhaelee <jhealee0624@gmail.com>
2022-03-22 23:53:31 +0900

    정수 내림차순으로 배치하기

--- prac5.py
+++ prac5.py
@@ -1,9 +1,3 @@
 def solution(n):
-    nums = []
-    answer = ""
-    for i in range(len(str(n))):
-        nums.append(int(str(n)[i]))
-    nums.sort(reverse=True)
-    for i in nums:
-        answer += str(i)
-    return int(answer)
+    nums = sorted(str(int(n)),reverse=True)
+    return int(''.join(nums))
