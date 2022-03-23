e7c611f914c2c62a3cdf205d3030f7346363a1c6
jeonhaelee <jhealee0624@gmail.com>
2022-03-23 14:58:51 +0900

    행렬의 덧셈 1

---
+++ prac6.py
@@ -0,0 +1,14 @@
+def solution(arr1, arr2):
+    
+    answer = []
+    for i in range(len(arr1)):
+        answer.append([])
+        for j in range(len(arr1[0])):
+            answer[i].append(0)
+            
+    for i in range(len(arr1)):
+        for j in range(len(arr1[0])):
+            answer[i][j] = arr1[i][j] + arr2[i][j]
+            
+    return answer
+
