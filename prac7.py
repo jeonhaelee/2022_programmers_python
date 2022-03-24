618adc0ab4267af67cd4d9c55661e91561db8eb2
jeonhaelee <jhealee0624@gmail.com>
2022-03-24 21:40:27 +0900

    최소직사각형_fin

--- prac7.py
+++ prac7.py
@@ -1,39 +1,16 @@
+
+
 def solution(sizes):
     answer = 0
-    max_w = sizes[0][0]
-    max_h = sizes[0][1]
+    long = []
+    short = []
     for i in range(len(sizes)):
-        if sizes[i][0] <= max_w and sizes[i][1] <= max_h:
-            continue
-        elif sizes[i][0] <= max_h and sizes[i][1] <= max_w:
-            continue
-        elif sizes[i][0] >= max_w and sizes[i][1] >= max_h:
-            max_w = sizes[i][0]
-            max_h = sizes[i][1]  
-        elif sizes[i][0] >= max_h and sizes[i][1] >= max_w:
-            max_w = sizes[i][1]
-            max_h = sizes[i][0]
-        elif sizes[i][0] >= max_w and sizes[i][1] <= max_h:
-            if sizes[i][1] <= max_w:
-                if sizes[i][0] * max_h <= max_w * sizes[i][0]:
-                    max_w = sizes[i][0]
-                else : max_h = sizes[i][0]
-            else : 
-                if max_h >= sizes[i][1]:
-                    max_w = sizes[i][0]
-                else : max_w = sizes[i][0]; max_h = sizes[i][1]
-        elif sizes[i][0] <= max_w and sizes[i][1] >= max_h:
-            if sizes[i][0] <= max_h:
-                if max_w * sizes[i][1] <= sizes[i][1] * max_h:
-                    max_h = sizes[i][1]
-                else : max_w = sizes[i][1]
-            else : 
-                if max_w >= sizes[i][0]:
-                    max_h = sizes[i][1]
-                else : max_w = sizes[i][0]; max_h = sizes[i][1]
-
-    answer = max_h * max_w
+        if sizes[i][0] >= sizes[i][1]:
+            long.append(sizes[i][0])
+            short.append(sizes[i][1])
+        else : 
+            long.append(sizes[i][1])
+            short.append(sizes[i][0])
+    answer = max(long) * max(short)
     return answer
 
-sizes = [[60, 50], [30, 70], [60, 30], [80, 40]]
-print(solution(sizes))
