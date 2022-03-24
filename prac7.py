d30c00560f7c8e74daa1e6708ce049cd840d0e1b
jeonhaelee <jhealee0624@gmail.com>
2022-03-24 18:59:54 +0900

    최소직사각형 2

--- prac7.py
+++ prac7.py
@@ -4,11 +4,21 @@ def solution(sizes):
     max_h = sizes[0][1]
     for i in range(len(sizes)):
         if sizes[i][0] >= max_w:
-            max_w = sizes[i][0]
+            if sizes[i][0] <= max_h and sizes[i][1] <= max_w:
+                continue
+            if sizes[i][0]-max_w >= sizes[i][0]-max_h:
+                max_h = sizes[i][0]
+            else : max_w = sizes[i][0]
         if sizes[i][1] >= max_h:
-            max_h = sizes[i][1]
+            if sizes[i][0] <= max_h and sizes[i][1] <= max_w:
+                continue
+            if sizes[i][1]-max_h >= sizes[i][1]-max_w:
+                max_w = sizes[i][1]
+                if sizes[i][0] >= max_h:
+                    max_h = sizes[i][0]
+            else : max_h = sizes[i][1]
     answer = max_h * max_w
     return answer
 
-sizes = [[60, 50], [30, 70], [60, 30], [80, 40]]
-solution(sizes)
+sizes = [[10, 7], [12, 3], [8, 15], [14, 7], [5, 15]]
+print(solution(sizes))
