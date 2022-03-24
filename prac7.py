44aba88e6c29285a3261404e16daae71a827c8e2
jeonhaelee <jhealee0624@gmail.com>
2022-03-24 21:31:07 +0900

    최소직사각형 4

--- prac7.py
+++ prac7.py
@@ -10,13 +10,30 @@ def solution(sizes):
         elif sizes[i][0] >= max_w and sizes[i][1] >= max_h:
             max_w = sizes[i][0]
             max_h = sizes[i][1]  
+        elif sizes[i][0] >= max_h and sizes[i][1] >= max_w:
+            max_w = sizes[i][1]
+            max_h = sizes[i][0]
         elif sizes[i][0] >= max_w and sizes[i][1] <= max_h:
-            max_w = sizes[i][0]
+            if sizes[i][1] <= max_w:
+                if sizes[i][0] * max_h <= max_w * sizes[i][0]:
+                    max_w = sizes[i][0]
+                else : max_h = sizes[i][0]
+            else : 
+                if max_h >= sizes[i][1]:
+                    max_w = sizes[i][0]
+                else : max_w = sizes[i][0]; max_h = sizes[i][1]
         elif sizes[i][0] <= max_w and sizes[i][1] >= max_h:
-            max_h = sizes[i][1]
+            if sizes[i][0] <= max_h:
+                if max_w * sizes[i][1] <= sizes[i][1] * max_h:
+                    max_h = sizes[i][1]
+                else : max_w = sizes[i][1]
+            else : 
+                if max_w >= sizes[i][0]:
+                    max_h = sizes[i][1]
+                else : max_w = sizes[i][0]; max_h = sizes[i][1]
 
     answer = max_h * max_w
     return answer
 
-sizes = [[10, 7], [12, 3], [8, 15], [14, 7], [5, 15]]
+sizes = [[60, 50], [30, 70], [60, 30], [80, 40]]
 print(solution(sizes))
