c1dfde72d5a504ec112c60c69a5549506bd00b1f
jeonhaelee <jhealee0624@gmail.com>
2022-03-24 20:42:37 +0900

    최소직사각형 3

--- prac7.py
+++ prac7.py
@@ -3,20 +3,18 @@ def solution(sizes):
     max_w = sizes[0][0]
     max_h = sizes[0][1]
     for i in range(len(sizes)):
-        if sizes[i][0] >= max_w:
-            if sizes[i][0] <= max_h and sizes[i][1] <= max_w:
-                continue
-            if sizes[i][0]-max_w >= sizes[i][0]-max_h:
-                max_h = sizes[i][0]
-            else : max_w = sizes[i][0]
-        if sizes[i][1] >= max_h:
-            if sizes[i][0] <= max_h and sizes[i][1] <= max_w:
-                continue
-            if sizes[i][1]-max_h >= sizes[i][1]-max_w:
-                max_w = sizes[i][1]
-                if sizes[i][0] >= max_h:
-                    max_h = sizes[i][0]
-            else : max_h = sizes[i][1]
+        if sizes[i][0] <= max_w and sizes[i][1] <= max_h:
+            continue
+        elif sizes[i][0] <= max_h and sizes[i][1] <= max_w:
+            continue
+        elif sizes[i][0] >= max_w and sizes[i][1] >= max_h:
+            max_w = sizes[i][0]
+            max_h = sizes[i][1]  
+        elif sizes[i][0] >= max_w and sizes[i][1] <= max_h:
+            max_w = sizes[i][0]
+        elif sizes[i][0] <= max_w and sizes[i][1] >= max_h:
+            max_h = sizes[i][1]
+
     answer = max_h * max_w
     return answer
 
