07420cbebcee1ad6be7d6bd6306e60558d10f2ca
jeonhaelee <jhealee0624@gmail.com>
2022-04-06 01:14:00 +0900

    더 맵게 3

--- prac19.py
+++ prac19.py
@@ -1,4 +1,29 @@
 
+# def is_scov_all_k(scoville, k):
+#     for i in scoville:
+#         if i < k:
+#             return False
+#     return True
+
+# def solution(scoville, k):
+#     count = 0
+#     scoville.sort()
+#     while is_scov_all_k(scoville, k) == False:
+#         scoville[0] = scoville[0] + (scoville[1] * 2)
+#         del scoville[1]
+#         count += 1
+#         if len(scoville) == 1:
+#             if is_scov_all_k(scoville, k) == False:
+#                 return -1
+#     return count
+
+# scoville = [1, 2, 3, 9, 10, 12]
+# k = 7
+# print(solution(scoville, k))
+
+
+import heapq as h
+
 def is_scov_all_k(scoville, k):
     for i in scoville:
         if i < k:
@@ -7,19 +32,15 @@ def is_scov_all_k(scoville, k):
 
 def solution(scoville, k):
     count = 0
-    scoville.sort()
     while is_scov_all_k(scoville, k) == False:
-        scoville[0] = scoville[0] + (scoville[1] * 2)
-        del scoville[1]
+        num = h.heappop(scoville)
+        num += h.heappop(scoville)*2
+        h.heappush(scoville,num)
         count += 1
         if len(scoville) == 1:
             if is_scov_all_k(scoville, k) == False:
                 return -1
     return count
-
-
-
-
 
 scoville = [1, 2, 3, 9, 10, 12]
 k = 7
