8047eed63b4676eae2c76730c7ea5213969ce4eb
jeonhaelee <jhealee0624@gmail.com>
2022-04-11 20:47:26 +0900

    124 나라의 숫자 4

--- prac21.py
+++ prac21.py
@@ -34,32 +34,8 @@ def solution(num):
             sub_answer += '2'
         elif 7 <= number and number <= 9:
             sub_answer += '4'
-    
-    # num, rest = divmod(num, 3)
-    # if rest == 1:
-    #     sub_answer += '1'
-    # elif rest == 2:
-    #     sub_answer += '2'
-    # else: sub_answer += '4'
-    
-    # while num > 3:
-    #     num, rest = divmod(num, 3)
-    #     if rest == 1:
-    #         sub_answer += '1'
-    #     elif rest == 2:
-    #         sub_answer += '2'
-    #     else: sub_answer += '4'
-    # if num == 1:
-    #     sub_answer += '1'
-    # elif num == 2:
-    #     sub_answer += '2'
-    # else: sub_answer += '4'
         
     for i in range(len(sub_answer)-1,-1,-1):
         answer += sub_answer[i]
         
     return int(answer)
-
-
-n = 13
-print(solution(n))
