523d0833fb0614ff2f98697c5d24f1499947525c
jeonhaelee <jhealee0624@gmail.com>
2022-04-24 17:58:02 +0900

    전화번호 목록_nonfin

--- prac22.py
+++ prac22.py
@@ -3,11 +3,11 @@
 def solution(phone_book):
     answer = True
     phone_book.sort()
-    print(phone_book)
     for num in range(len(phone_book)):
+        set = phone_book[num]
         for n in range(num+1, len(phone_book)):
             number = str(phone_book[n])
-            if number[:len(str(num))] == str(num):
+            if number[:len(str(set))] == str(set):
                 answer = False
                 return answer
     return answer
