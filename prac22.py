a316e9a91529ac6e87b6b4f622fb38d062439e59
jeonhaelee <jhealee0624@gmail.com>
2022-04-24 18:44:52 +0900

    전화번호 목록 _ nonfin

--- prac22.py
+++ prac22.py
@@ -1,15 +1,22 @@
 
+# def solution(phone_book):
+#     answer = True
+#     phone_book.sort()
+#     phone_book = str(phone_book)
+#     for i in range(len(phone_book)-1):
+#         if phone_book[i+1].startswith(phone_book[i]):
+#             answer = False
+#             return answer
+#     return answer
 
 def solution(phone_book):
     answer = True
     phone_book.sort()
-    for num in range(len(phone_book)):
-        set = phone_book[num]
-        for n in range(num+1, len(phone_book)):
-            number = str(phone_book[n])
-            if number[:len(str(set))] == str(set):
-                answer = False
-                return answer
+    phone_book = str(phone_book)
+    for i in range(len(phone_book)-1):
+        if phone_book[i+1:].startswith(phone_book[i]):
+            answer = False
+            return answer
     return answer
 
 phone_book = ["934793", "34", "44", "9347"]
