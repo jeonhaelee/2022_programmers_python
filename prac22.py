fd67a9e9ea29a813ac6685e785f64a876cca6e66
jeonhaelee <jhealee0624@gmail.com>
2022-04-24 17:50:48 +0900

    전화번호 목록_nonfin

--- prac22.py
+++ prac22.py
@@ -7,7 +7,7 @@ def solution(phone_book):
     for num in range(len(phone_book)):
         for n in range(num+1, len(phone_book)):
             number = str(phone_book[n])
-            if number.startswith(str(num)):
+            if number[:len(str(num))] == str(num):
                 answer = False
                 return answer
     return answer
