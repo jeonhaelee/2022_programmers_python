d4674652b98aa05b99243ef69e7d20861517a8d6
jeonhaelee <jhealee0624@gmail.com>
2022-04-25 23:50:09 +0900

    프린터_fin

--- prac23.py
+++ prac23.py
@@ -1,24 +1,17 @@
 
 
-from re import S
-
-from numpy import take
-
-
 def solution(priorities, location):
     answer = 0
     
     tasks = []
     for i in range(len(priorities)):
         tasks.append((priorities[i], i))
-    print(tasks)
     
     value = tasks[location]
-    print("찾으려는 값 : {}".format(value))
     while value in tasks:
         temp_list = sorted(tasks, key= lambda x : -x[0])
         max_val = temp_list[0][0]
-        print(max_val)
+
         count = 0
         for val in tasks:
             if val[0] == max_val:
@@ -32,7 +25,6 @@ def solution(priorities, location):
         else: 
             del tasks[0]
             answer += 1
-        print(tasks)
         
     return answer
 
