2d633171ecfed096d54133990fa5099c15fa0696
jeonhaelee <jhealee0624@gmail.com>
2022-04-26 20:51:06 +0900

    짝지어 제거하기_nonfin

--- prac24.py
+++ prac24.py
@@ -10,11 +10,19 @@ def solution(s):
     result = []
     for i in range(len(s)):
         result += s[i]
+    print("초기 result 값 : {}".format(result))
     while find_test(result) :
         for i in range(len(result)-1):
+            if i >= len(result):
+                break
+            print("i 값 : {}".format(i))
+            print("현재 result : {}".format(result))
             if result[i] == result[i+1]:
+                print("result 제거 전 : {}".format(result))
                 del result[i]
-                del result[i+1]
+                del result[i]
+                print("result 제거 후 : {}".format(result))
+    print("최종 result 값 : {}".format(result))
     if len(result) == 0:
         answer = 1
         return answer
