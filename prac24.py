97a7fbc25051147325c5deeabcfdfa3c9db5d073
jeonhaelee <jhealee0624@gmail.com>
2022-04-26 20:39:28 +0900

    짝지어 제거하기_nonfin

--- prac24.py
+++ prac24.py
@@ -1,5 +1,29 @@
+
+from unittest import result
+
+
+def find_test(sub_result):
+    for i in range(len(sub_result)-1):
+        if result[i] == result[i+1]:
+            return True
+    return False
+
 def solution(s):
     answer = -1
+    result = []
+    for i in range(len(s)):
+        result += s[i]
+    while find_test(result) :
+        for i in range(len(result)-1):
+            if result[i] == result[i+1]:
+                del result[i]
+                del result[i+1]
+    if len(result) == 0:
+        answer = 1
+        return answer
+    else:
+        answer = 0
+        return answer
 
 
 
