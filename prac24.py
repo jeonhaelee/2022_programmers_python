b606f6634e27a2759fdccb1b447cab9c67946493
jeonhaelee <jhealee0624@gmail.com>
2022-04-26 20:57:32 +0900

    짝지어 제거하기_nonfin

--- prac24.py
+++ prac24.py
@@ -5,18 +5,43 @@ def find_test(result):
             return True
     return False
 
+# def solution(s):
+#     answer = -1
+#     result = []
+#     for i in range(len(s)):
+#         result += s[i]
+#     while find_test(result) :
+#         for i in range(len(result)-1):
+#             if i >= len(result):
+#                 break
+#             if result[i] == result[i+1]:
+#                 del result[i]
+#                 del result[i]
+#     if len(result) == 0:
+#         answer = 1
+#         return answer
+#     else:
+#         answer = 0
+#         return answer
+
+
 def solution(s):
     answer = -1
     result = []
     for i in range(len(s)):
         result += s[i]
+    print("초기 result : {}".format(result))
+    i = 0
     while find_test(result) :
-        for i in range(len(result)-1):
-            if i >= len(result):
-                break
-            if result[i] == result[i+1]:
-                del result[i]
-                del result[i]
+        if result[i] == result[i+1]:
+            print("result 제거 전 : {}".format(result))
+            del result[i]
+            del result[i]
+            print("result 제거 후 : {}".format(result))
+        i += 1
+        if i >= len(result):
+            i = 0
+    print("최종 result : {}".format(result))
     if len(result) == 0:
         answer = 1
         return answer
@@ -24,5 +49,5 @@ def solution(s):
         answer = 0
         return answer
 
-s = "cdcd"
+s = "baabaa"
 print(solution(s))
