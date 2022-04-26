9e6b52e8a0157c3cfd0c1f7aade6b22ef9b59cc2
jeonhaelee <jhealee0624@gmail.com>
2022-04-26 21:02:13 +0900

    짝지어 제거하기_nonfin

--- prac24.py
+++ prac24.py
@@ -5,6 +5,25 @@ def find_test(result):
             return True
     return False
 
+
+def solution(s):
+    answer = 0
+    
+    result = []
+    for i in range(len(s)):
+        result += s[i]
+        
+    while find_test(result) :
+        
+    if len(result) == 0:
+        answer = 1
+        return answer
+    else : return answer
+
+
+
+
+
 # def solution(s):
 #     answer = -1
 #     result = []
@@ -25,29 +44,29 @@ def find_test(result):
 #         return answer
 
 
-def solution(s):
-    answer = -1
-    result = []
-    for i in range(len(s)):
-        result += s[i]
-    print("초기 result : {}".format(result))
-    i = 0
-    while find_test(result) :
-        if result[i] == result[i+1]:
-            print("result 제거 전 : {}".format(result))
-            del result[i]
-            del result[i]
-            print("result 제거 후 : {}".format(result))
-        i += 1
-        if i >= len(result):
-            i = 0
-    print("최종 result : {}".format(result))
-    if len(result) == 0:
-        answer = 1
-        return answer
-    else:
-        answer = 0
-        return answer
+# def solution(s):
+#     answer = -1
+#     result = []
+#     for i in range(len(s)):
+#         result += s[i]
+#     print("초기 result : {}".format(result))
+#     i = 0
+#     while find_test(result) :
+#         if result[i] == result[i+1]:
+#             print("result 제거 전 : {}".format(result))
+#             del result[i]
+#             del result[i]
+#             print("result 제거 후 : {}".format(result))
+#         i += 1
+#         if i >= len(result):
+#             i = 0
+#     print("최종 result : {}".format(result))
+#     if len(result) == 0:
+#         answer = 1
+#         return answer
+#     else:
+#         answer = 0
+#         return answer
 
 s = "baabaa"
 print(solution(s))
