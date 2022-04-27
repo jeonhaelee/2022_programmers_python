6c5095a5276b47f2363f5734c0a2908725936ae8
jeonhaelee <jhealee0624@gmail.com>
2022-04-27 22:47:03 +0900

    소수 찾기_nonfin

--- prac25.py
+++ prac25.py
@@ -1,35 +1,50 @@
 from itertools import permutations
 
-def is_prime(n):
-    if n == 0 or n == 1:
-        return False
-    for i in range(2, n):
-        if n % i == 0:
-            return False
-    return True
-
-def prime_count(sub_nums, i):
-    count = 0
-    for n in sub_nums:
-        word = ""
-        for i in range(i):
-            word += n[i]
-        number = int(word)
-        if is_prime(number):
-            count += 1
-    return count
+class Stack :
+    def __init__(self) -> None:
+        self.stack = []
     
-def solution(numbers):
-    answer = 0
-    nums = []
-    for i in range(len(numbers)):
-        nums.append(numbers[i])
-    print("nums : {}".format(nums))
-    for i in range(1, len(nums)+1):
-        sub_nums = permutations(nums, i)
-        print("sub_nums : {}".format(sub_nums))
-        answer += prime_count(sub_nums, i)
-    return answer
+    def is_prime(self,n):
+        if n == 0 or n == 1:
+            return False
+        for i in range(2, n):
+            if n % i == 0:
+                return False
+        return True
 
-numbers = "17"
-print(solution(numbers))
+    def prime_count(self,sub_nums, i):
+        count = 0
+        for n in sub_nums:
+            word = ""
+            for i in range(len(n)):
+                word += n[i]
+            number = int(word)
+            if self.is_prime(number):
+                count += 1
+        return count
+        
+    def solution(self, numbers):
+        answer = 0
+        
+        nums = []
+        for i in range(len(numbers)):
+            nums.append(numbers[i])
+        print("nums : {}".format(nums))
+        
+        for i in range(len(nums)):
+            if i in self.stack:
+                continue
+            else:
+                if self.is_prime(int(i)):
+                    self.stack.append(i)
+                    answer += 1
+                
+        for i in range(2, len(nums)+1):
+            sub_nums = list(permutations(nums, i))
+            print("{}개씩 sub_nums : {}".format(i, sub_nums))
+            answer += self.prime_count(sub_nums, i)
+            
+        return answer
+
+numbers = "011"
+print(Stack.solution(numbers))
