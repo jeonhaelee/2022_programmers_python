59003ac77beef3f9d0f4f6105b650a440b8ed95d
jeonhaelee <jhealee0624@gmail.com>
2022-04-28 22:08:28 +0900

    주식가격_nonfin

--- prac26.py
+++ prac26.py
@@ -1,5 +1,13 @@
+
+
 def solution(prices):
     answer = []
+    for i in range(len(prices)):
+        count = 0
+        for j in range(i+1, len(prices)):
+            if prices[i] <= prices[j]:
+                count += 1
+        answer.append(count)
     return answer
 
 prices = [1, 2, 3, 2, 3]
