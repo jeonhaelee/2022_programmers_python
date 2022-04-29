6462e63aaeb43c6618c3da09e11c1fd6b0c5f0d8
jeonhaelee <jhealee0624@gmail.com>
2022-04-29 23:07:02 +0900

    행렬의 곱셈_nonfin

--- prac27.py
+++ prac27.py
@@ -1,20 +1,33 @@
-import numpy as np
+# import numpy as np
+
+# def solution(arr1, arr2):
+#     answer = [[]]
+#     arr1 = np.array(arr1, dtype=np.int64)
+#     arr2 = np.array(arr2, dtype=np.int64)
+#     result = arr1.dot(arr2)
+
+#     answer = []
+#     for i in range(len(result)):
+#         sub_answer = []
+#         for j in range(len(result[0])):
+#             sub_answer.append(result[i,j])
+#         answer.append(sub_answer)
+#     return answer
+
 
 def solution(arr1, arr2):
     answer = [[]]
-    arr1 = np.array(arr1, dtype=np.int64)
-    arr2 = np.array(arr2, dtype=np.int64)
-    result = arr1.dot(arr2)
-    
-    answer = []
-    for i in range(len(result)):
-        sub_answer = []
-        for j in range(len(result[0])):
-            sub_answer.append(result[i,j])
-        answer.append(sub_answer)
+    for i in range(len(arr1)):
+        for j in range(len(arr1[0])):
+            for k in range(len(arr2[0])):
+                val = arr1[i][j] * arr2[j][k]
+                print(val)
+                answer[i].append(val)
+                print(answer[i])
+    print(answer)
     return answer
 
-arr1 = [[2, 3, 2], [4, 2, 4], [3, 1, 4]]
-arr2 = [[5, 4, 3], [2, 4, 1], [3, 1, 1]]
+arr1 = [[1, 4], [3, 2], [4, 1]]
+arr2 = [[3, 3], [3, 3]]
 print(solution(arr1, arr2))
 
