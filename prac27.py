016aa5a51f1e8cc8b4f5942b7846f6cdb6282e58
jeonhaelee <jhealee0624@gmail.com>
2022-04-29 22:55:32 +0900

    행렬의 곱셈_nonfin

--- prac27.py
+++ prac27.py
@@ -1,11 +1,19 @@
 import numpy as np
 
 def solution(arr1, arr2):
+    answer = [[]]
     arr1 = np.array(arr1)
     arr2 = np.array(arr2)
-    answer = arr1.dot(arr2)
+    result = arr1.dot(arr2)
+    answer = []
+    for i in range(len(result)):
+        sub_answer = []
+        for j in range(len(result[0])):
+            sub_answer.append(result[i,j])
+        answer.append(sub_answer)
     return answer
 
-arr1 = [[1, 4], [3, 2], [4, 1]]
-arr2 = [[3, 3], [3, 3]]
+arr1 = [[2, 3, 2], [4, 2, 4], [3, 1, 4]]
+arr2 = [[5, 4, 3], [2, 4, 1], [3, 1, 1]]
 print(solution(arr1, arr2))
+
