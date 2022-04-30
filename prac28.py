dd2cf6fa6aa5d0b76816430a13872712892c0d78
jeonhaelee <jhealee0624@gmail.com>
2022-04-30 23:00:03 +0900

    카펫_fin

--- prac28.py
+++ prac28.py
@@ -2,14 +2,9 @@ import math
 
 def solution(brown, yellow):
     answer = []
-    x = 0; y = 0
     value = (brown - 4)/2
     x = (value + math.sqrt(pow(value,2)-4*yellow))/2
     y = value - x
     answer.append(int(x)+2)
     answer.append(int(y)+2)
     return answer
-
-brown = 24
-yellow = 24
-print(solution(brown, yellow))
