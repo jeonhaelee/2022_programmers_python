e73ee945d6385c4f51ebad18cbbad5b0803ef604
jeonhaelee <jhealee0624@gmail.com>
2022-04-30 22:56:10 +0900

    카펫_nonfin

--- prac28.py
+++ prac28.py
@@ -1,5 +1,13 @@
+import math
+
 def solution(brown, yellow):
     answer = []
+    x = 0; y = 0
+    value = (brown - 4)/2
+    x = (value + math.sqrt(pow(value,2)-4*yellow))/2
+    y = 3 - x
+    answer.append(int(x)+2)
+    answer.append(int(y)+2)
     return answer
 
 brown = 10
