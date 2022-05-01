63c792dc8328cae7b8dd32157eefc8f7249cfbb5
jeonhaelee <jhealee0624@gmail.com>
2022-05-01 16:46:05 +0900

    행렬의 곱셈_fin

--- prac27.py
+++ prac27.py
@@ -1,8 +1,10 @@
 
 
 def solution(arr1, arr2):
+    
     h = len(arr1) # arr1 행 길이
     w = len(arr2[0]) # arr2 열 길이
+    
     answer = []
     for i in range(h):
         sub_answer = []
@@ -12,12 +14,8 @@ def solution(arr1, arr2):
         
     for i in range(h):
         for j in range(w):
-            for k in range(h):
+            for k in range(len(arr1[0])):
                 answer[i][j] += arr1[i][k] * arr2[k][j]
     
     return answer
-
-arr1 = [[1, 4], [3, 2], [4, 1]]
-arr2 = [[3, 3], [3, 3]]
-print(solution(arr1, arr2))
 
