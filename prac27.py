cd21aa87d866c52613593d82e2355ba6ec2a592a
jeonhaelee <jhealee0624@gmail.com>
2022-05-01 16:43:36 +0900

    행렬의 곱셈_nonfin

--- prac27.py
+++ prac27.py
@@ -1,8 +1,8 @@
 
 
 def solution(arr1, arr2):
-    h = len(arr1)
-    w = len(arr2[0])
+    h = len(arr1) # arr1 행 길이
+    w = len(arr2[0]) # arr2 열 길이
     answer = []
     for i in range(h):
         sub_answer = []
@@ -11,8 +11,8 @@ def solution(arr1, arr2):
         answer.append(sub_answer)
         
     for i in range(h):
-        for j in range(len(arr2)):
-            for k in range(w):
+        for j in range(w):
+            for k in range(h):
                 answer[i][j] += arr1[i][k] * arr2[k][j]
     
     return answer
