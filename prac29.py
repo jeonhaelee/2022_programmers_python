906af6357f1830ac28bd89936478af6fbf144aa9
jeonhaelee <jhealee0624@gmail.com>
2022-05-02 23:33:59 +0900

    최댓값과 최솟값_nonfin

--- prac29.py
+++ prac29.py
@@ -1,7 +1,15 @@
 def solution(s):
     answer = ''
     d = list(map(int, s.split(" ")))
-    print(d)
+    min = d[0]; max = d[0]
+    for num in d:
+        if num < min :
+            min = num
+        if num > max :
+            max = num
+    answer += str(min)
+    answer += " "
+    answer += str(max)
     return answer
 
 s = "-1 -2 -3 -4"
