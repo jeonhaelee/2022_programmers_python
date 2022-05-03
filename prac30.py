7d8c58282329d2807d4790260bf4947eddf722dd
jeonhaelee <jhealee0624@gmail.com>
2022-05-03 22:47:43 +0900

    숫자의 표현_nonfin

--- prac30.py
+++ prac30.py
@@ -1,5 +1,17 @@
 def solution(n):
     answer = 0
+    start_num = 1
+    while start_num <= n:
+        sub_ans = 0
+        for i in range(start_num, n+1):
+            sub_ans += i
+            if sub_ans == n:
+                start_num += 1
+                answer += 1
+                break
+            if sub_ans > n:
+                start_num += 1
+                break
     return answer
 
 n = 15
