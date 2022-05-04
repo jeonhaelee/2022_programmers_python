45ccc6eeaaa4f669b737dc78e17867f91514523c
jeonhaelee <jhealee0624@gmail.com>
2022-05-04 20:56:37 +0900

    JadenCase 문자열 만들기_nonfin

--- prac31.py
+++ prac31.py
@@ -1,11 +1,16 @@
 def solution(s):
     answer = ''
     words = s.split(" ")
-    for word in range(words):
+    for word in words:
         for i in range(len(word)):
-            if word.isdigit() :
-                answer += i
-            elif word.
+            if i == 0:
+                if word[0].islower():
+                    answer += word[0].upper()
+                else: 
+                    answer += word[0]
+            else:
+                answer += word[i]
+        answer += " "
     return answer
 
 s = "3people unFollowed me"
