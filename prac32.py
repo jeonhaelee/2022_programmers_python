e5d74b1345a2b7c0c9ba9d4c0897f1322fe33033
jeonhaelee <jhealee0624@gmail.com>
2022-05-05 19:36:51 +0900

    피보나치 수_nonfin

--- prac32.py
+++ prac32.py
@@ -1,6 +1,10 @@
-def solution(n):
-    answer = 0
+def pivo(n):
+    answer = pivo(n-2) + pivo(n-1)
     return answer
+
+def solution(n):
+    answer = pivo(n)
+    return int(answer/1234567)
 
 n = 3
 print(solution(n))
