e8df36a16b6f90e86e88ee20161aff9b951d25fe
jeonhaelee <jhealee0624@gmail.com>
2022-05-07 23:50:13 +0900

    튜플_nonfin

--- prac33.py
+++ prac33.py
@@ -2,12 +2,14 @@ def solution(s):
     answer = [] # answer의 크기는 len(s)
     d = []
     sub = []
-    for i in range(len(s)):
+    for i in range(len(s)-1):
         if s[i].isdigit():
             sub += s[i]
         if s[i] == "}":
             d.append(sub)
             sub = []
+            if s[i+1] == "}":
+                break
     print(d)
     return answer
 
