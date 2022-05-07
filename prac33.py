82f31752e7fb58e330a34da0cd14d0105ab87e8b
jeonhaelee <jhealee0624@gmail.com>
2022-05-07 23:48:27 +0900

    튜플_nonfin

--- prac33.py
+++ prac33.py
@@ -1,5 +1,14 @@
 def solution(s):
     answer = [] # answer의 크기는 len(s)
+    d = []
+    sub = []
+    for i in range(len(s)):
+        if s[i].isdigit():
+            sub += s[i]
+        if s[i] == "}":
+            d.append(sub)
+            sub = []
+    print(d)
     return answer
 
 s = "{{2},{2,1},{2,1,3},{2,1,3,4}}"
