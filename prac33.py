0fdc22d2551641e360b3cc1b7c8598fc0652b892
jeonhaelee <jhealee0624@gmail.com>
2022-05-09 00:04:30 +0900

    튜플_nonfin

--- prac33.py
+++ prac33.py
@@ -1,21 +1,4 @@
-# def solution(s):
-#     answer = [] # answer의 크기는 len(s)
-#     d = []
-#     sub = []
-#     for i in range(len(s)-1):
-#         if s[i].isdigit():
-#             sub += s[i]
-#         if s[i] == "}":
-#             d.append(sub)
-#             sub = []
-#             if s[i+1] == "}":
-#                 break
-#     print(d)
-#     for i in range(len(d)):
-#         for j in range(len(d[i])):
-#             if d[i][j] not in answer:
-#                 answer.append(d[i][j])
-#     return answer
+
 
 def solution(s):
     answer = [] # answer의 크기는 len(s)
@@ -33,6 +16,9 @@ def solution(s):
     print(d)
     s2 = d[-1][0]
     print(s2)
+    for num in s2:
+        answer.append(num)
+    print(answer)
     return answer
 
 s = "{{2},{2,1},{2,1,3},{2,1,3,4}}"
