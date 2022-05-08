857a58eb49640bca0e9c583c0df7b60bc073aaec
jeonhaelee <jhealee0624@gmail.com>
2022-05-08 23:59:42 +0900

    튜플_nonfin

--- prac33.py
+++ prac33.py
@@ -2,21 +2,20 @@ def solution(s):
     answer = [] # answer의 크기는 len(s)
     d = []
     sub = []
-    num = ""
     for i in range(len(s)-1):
         if s[i].isdigit():
-            num += s[i]
-        if s[i] == ",":
-            d.append(int(num))
+            sub += s[i]
         if s[i] == "}":
             d.append(sub)
             sub = []
             if s[i+1] == "}":
                 break
     print(d)
-    for num in d[-1]:
-        answer.append(num)
+    for i in range(len(d)):
+        for j in range(len(d[i])):
+            if d[i][j] not in answer:
+                answer.append(d[i][j])
     return answer
 
-s = "{{20,111},{111}}"
+s = "{{2},{2,1},{2,1,3},{2,1,3,4}}"
 print(solution(s))
