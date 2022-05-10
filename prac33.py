d3de6baa935374d1edc0ba67b6d2482faf6b794f
jeonhaelee <jhealee0624@gmail.com>
2022-05-10 18:11:10 +0900

    튜플_nonfin

--- prac33.py
+++ prac33.py
@@ -1,24 +1,51 @@
 
 
+# def solution(s):
+#     answer = [] # answer의 크기는 len(s)
+#     d = []
+#     sub = []
+#     num = ""
+#     for i in range(len(s)-1):
+#         if s[i].isdigit():
+#             num += s[i]
+#             if s[i+1] == "," or s[i+1] == "}":
+#                 sub.append(int(num))
+#                 num = ""
+#         elif s[i] == "}":
+#             d.append((sub, len(sub)))
+#             sub = []
+#             if s[i+1] == "}":
+#                 break
+#     d.sort(key=lambda x:x[1])
+#     s2 = d[-1][0]
+#     for num in s2:
+#         answer.append(num)
+#     return answer
+
+
+
 def solution(s):
     answer = [] # answer의 크기는 len(s)
     d = []
     sub = []
     num = ""
-    for i in range(len(s)-1):
+    for i in range(1, len(s)-1):
+        if s[i] == "{":
+            sub = []
         if s[i].isdigit():
             num += s[i]
-            if s[i+1] == "," or s[i+1] == "}":
-                sub.append(int(num))
-                num = ""
-        elif s[i] == "}":
+        if s[i] == ",":
+            sub.append(int(num))
+            num = ""
+        if s[i] == "}":
             d.append((sub, len(sub)))
             sub = []
             if s[i+1] == "}":
                 break
-        print(d)
     d.sort(key=lambda x:x[1])
+    print(d)
     s2 = d[-1][0]
+    print(s2)
     for num in s2:
         answer.append(num)
     return answer
