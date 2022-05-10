b8ac0014d1c1a6bedd32924004e773685407469b
jeonhaelee <jhealee0624@gmail.com>
2022-05-10 19:32:50 +0900

    튜플_fin

--- prac33.py
+++ prac33.py
@@ -1,34 +1,7 @@
 
-
-# def solution(s):
-#     answer = [] # answer의 크기는 len(s)
-#     d = []
-#     sub = []
-#     num = ""
-#     for i in range(len(s)-1):
-#         if s[i].isdigit():
-#             num += s[i]
-#             if s[i+1] == "," or s[i+1] == "}":
-#                 sub.append(int(num))
-#                 num = ""
-#         elif s[i] == "}":
-#             d.append((sub, len(sub)))
-#             sub = []
-#             if s[i+1] == "}":
-#                 break
-#     d.sort(key=lambda x:x[1])
-#     s2 = d[-1][0]
-#     for num in s2:
-#         answer.append(num)
-#     return answer
-
-
-
 def solution(s):
     answer = [] # answer의 크기는 len(s)
-    d = []
-    sub = []
-    num = ""
+    d = []; sub = []; num = ""
     for i in range(1, len(s)-1):
         if s[i] == "{":
             sub = []
@@ -45,12 +18,9 @@ def solution(s):
             d.append(sub)
             if s[i+1] == "}":
                 break
-    d.sort(key=len)
+    d.sort(key=len) # d 리스트 안의 sub 리스트 길이순으로 정렬하기
     for nums in d:
         for num in nums:
             if num not in answer:
                 answer.append(num)
     return answer
-
-s = "{{1,2,3},{2,1},{1,2,4,3},{2}}"
-print(solution(s))
