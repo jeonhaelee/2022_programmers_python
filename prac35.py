69813135e68dd046acaadc6435f7b9ba76fe5a1a
jeonhaelee <jhealee0624@gmail.com>
2022-05-11 23:08:14 +0900

    위장_nonfin

--- prac35.py
+++ prac35.py
@@ -1,6 +1,22 @@
+import numpy as np
+
 def solution(clothes):
-    answer = 0
-    return answer
+    answer = 1
+
+    type = []
+    for cloth in clothes:
+      if cloth[1] not in type:
+        type.append(cloth[1])
+
+    type_num = np.zeros(len(type), dtype = np.int8)
+    for cloth in clothes:
+      idx = type.index(cloth[1])
+      type_num[idx] += 1
+
+    for num in type_num:
+      answer *= (int(num)+1)
+
+    return answer-1
 
 clothes = [["yellowhat", "headgear"], ["bluesunglasses", "eyewear"], ["green_turban", "headgear"]]
 print(solution(clothes))
