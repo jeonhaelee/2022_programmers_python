d97335195d6d8de0a899f3333f09599dee6d4044
jeonhaelee <jhealee0624@gmail.com>
2022-05-11 22:57:31 +0900

    스킬트리_fin

--- prac34.py
+++ prac34.py
@@ -1,5 +1,12 @@
 def solution(skill, skill_trees):
-    answer = -1
+    answer = 0
+    for words in skill_trees:
+      result = ""
+      for word in words:
+        if word in skill:
+          result += word
+      if skill.startswith(result):
+        answer += 1
     return answer
 
 skill = "CBD"
