1e2b91fb4982954ecf9faf7c91dcf7a9f116d9a9
jeonhaelee <jhealee0624@gmail.com>
2022-05-12 23:25:13 +0900

    H-index_fin

--- prac36.py
+++ prac36.py
@@ -1,4 +1,3 @@
-
 def count_num(h, citations):
     count = 0
     for num in citations:
@@ -8,22 +7,7 @@ def count_num(h, citations):
 
 def solution(citations):
     n = len(citations)
-    if n == 1:
-        return 0
     for h in range(n, -1, -1):
         if h <= count_num(h, citations):
             return h
-        if h == 0:
-            return 0
-
-citations = [0, 1, 1]
-print(solution(citations))
-
-# h는 n보다 작음
-# n은 citations의 길이
-
-#어떤 과학자가 발표한 논문 n편 중, 
-# h번 이상 인용된 논문이 h편 이상이고 나머지 논문이 h번 이하 인용되었다면 
-# h의 최댓값이 이 과학자의 H-Index입니다.
-
-# n편 중 h편 이상이 h번 이상 인용되었고, n-h 즉 나머지 논문은 h번 이하 인용됨
+    return 0
