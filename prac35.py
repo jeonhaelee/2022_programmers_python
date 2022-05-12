7b5dee5304700ddce2e3d700179336859a575539
jeonhaelee <jhealee0624@gmail.com>
2022-05-12 22:35:36 +0900

    위장_fin

--- prac35.py
+++ prac35.py
@@ -1,14 +1,13 @@
-import numpy as np
-
 def solution(clothes):
     answer = 1
     type = {}
+    
     for name, cloth_type in clothes:
       if cloth_type not in type:
         type[cloth_type] = 2
       else :
         type[cloth_type] += 1
-    print(type)
+        
     for num in type.values() :
       answer *= num
 
