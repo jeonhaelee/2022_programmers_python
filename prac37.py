f93f9223b89126e737c642b5169a268c4c6ce562
jeonhaelee <jhealee0624@gmail.com>
2022-05-13 23:21:21 +0900

    피보나치수_fin

--- prac37.py
+++ prac37.py
@@ -2,10 +2,17 @@ import sys
 sys.setrecursionlimit(10**7)
 
 def solution(n):
-    if n == 0: return 0
-    if n == 1: return 1
-    answer = solution(n-1) + solution(n-2)
-    return answer % 1234567
+    arr = [-1]*(n+1)
+    arr[0] = 0
+    arr[1] = 1
+    return find(arr, n) % 1234567
+
+def find(arr, n):   # 이미 구했었던 피보나치수라면, 찾아서 꺼내주는 식으로 시간 단축
+    if arr[n] != -1:
+      return arr[n]
+    else:
+      arr[n] =  find(arr, n-1) + find(arr, n-2)
+      return arr[n]
 
 n = 5
 print(solution(n))
