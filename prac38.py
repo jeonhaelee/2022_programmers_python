d33dc5286e15b9ab5b1bb9eecce32f50e1a14e04
jeonhaelee <jhealee0624@gmail.com>
2022-05-14 14:14:26 +0900

    다리를 지나는 트럭_nonfin

--- prac38.py
+++ prac38.py
@@ -1,5 +1,16 @@
+
+def weight_is_possible(bridge, weight):
+    sum = 0
+    for w in bridge:
+        sum += w
+    if sum <= weight:
+        return True
+    else: return False
+
 def solution(bridge_length, weight, truck_weights):
     answer = 0
+    bridge = [-1]*bridge_length
+
     return answer
 
 bridge_length = 2
