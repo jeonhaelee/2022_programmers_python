43ec414ddced541c86790200f864a8a8f125ac88
jeonhaelee <jhealee0624@gmail.com>
2022-05-14 15:38:29 +0900

    다리를 지나는 트럭_fin

--- prac38.py
+++ prac38.py
@@ -1,78 +1,35 @@
 
-# def weight_is_possible(bridge, weight):
-#     sum = 0
-#     for w in bridge:
-#         sum += w
-#     if sum <= weight:
-#         return True
-#     else: return False
-
-# def solution(bridge_length, weight, truck_weights):
-#     answer = 0
-#     bridge = [-1]*bridge_length
-    
-#     bridge[-1] = truck_weights[0]
-#     del truck_weights[0]
-#     answer += 1
-#     print("answer : {}".format(answer))
-    
-#     while len(truck_weights) > 0:
-#         bridge[0], bridge[1] = bridge[1], -1
-#         bridge[-1] = truck_weights[0]
-#         if weight_is_possible(bridge, weight) :
-#             del truck_weights[0]
-#             print("answer : {}".format(answer))
-#             print("bridge : {}".format(bridge))
-#             print("truck_weights : {}".format(truck_weights))
-#             answer += 1
-#         else:
-#             bridge[-1] = -1
-#             print("answer : {}".format(answer))
-#             print("bridge : {}".format(bridge))
-#             print("truck_weights : {}".format(truck_weights))
-#             answer += 1
-            
-#     answer += bridge_length
-#     return answer
-
-
-
-def weight_is_possible(bridge, weight, truck_weights):
-    sum = 0
-    for i in range(1, len(bridge)):
-        if bridge[i] != -1:
-            sum += bridge[i]
-    if sum + truck_weights[0] <= weight:
-        return True
-    else: return False
-
 def solution(bridge_length, weight, truck_weights):
     answer = 0
     bridge = [-1]*bridge_length
+    weight_sum = 0
     
     del bridge[0]
     bridge.append(truck_weights[0])
+    weight_sum += truck_weights[0]
     del truck_weights[0]
     answer += 1
-    print("answer : {}".format(answer))
-    print("bridge : {}".format(bridge))
-    print("truck_weights : {}".format(truck_weights))
     
     while len(truck_weights) > 0:
-        if weight_is_possible(bridge, weight, truck_weights) :
+        
+        if bridge[0] != -1:
+            test = weight_sum + truck_weights[0] - bridge[0]
+        else: test = weight_sum + truck_weights[0]
+        
+        if test <= weight :
+            if bridge[0] != -1:
+                weight_sum -= bridge[0]
             del bridge[0]
             bridge.append(truck_weights[0])
+            weight_sum += truck_weights[0]
             del truck_weights[0]
-            print("answer : {}".format(answer))
-            print("bridge : {}".format(bridge))
-            print("truck_weights : {}".format(truck_weights))
             answer += 1
+            
         else:
+            if bridge[0] != -1:
+                weight_sum -= bridge[0]
             del bridge[0]
             bridge.append(-1)
-            print("answer : {}".format(answer))
-            print("bridge : {}".format(bridge))
-            print("truck_weights : {}".format(truck_weights))
             answer += 1
             
     answer += bridge_length
