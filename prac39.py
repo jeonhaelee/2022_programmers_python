8ec038e4ed87a6b80dd3c703a1069b520449b870
jeonhaelee <jhealee0624@gmail.com>
2022-05-15 23:53:41 +0900

    최솟값 만들기_nonfin

--- prac39.py
+++ prac39.py
@@ -1,7 +1,11 @@
 def solution(A,B):
     answer = 0
-
+    while len(A) > 0:
+        answer += (min(A)*max(B))
+        del min(A)
+        del max(B)
     return answer
 
 A = [1, 4, 2]
 B = [5, 4, 4]	
+print(solution(A,B))
