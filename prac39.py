7930575eea83fb550d5df74c1f142f3190576207
jeonhaelee <jhealee0624@gmail.com>
2022-05-15 23:56:02 +0900

    최솟값 만들기_nonfin

--- prac39.py
+++ prac39.py
@@ -1,9 +1,14 @@
+from audioop import reverse
+
+
 def solution(A,B):
     answer = 0
+    A.sort()
+    B.sort(reverse=True)
     while len(A) > 0:
-        answer += (min(A)*max(B))
-        del min(A)
-        del max(B)
+        answer += (A[0]*B[0])
+        del A[0]
+        del B[0]
     return answer
 
 A = [1, 4, 2]
