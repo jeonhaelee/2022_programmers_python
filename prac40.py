fa7a19296fc53cee7d797c8cbba038d613cead9d
jeonhaelee <jhealee0624@gmail.com>
2022-05-16 23:29:20 +0900

    큰 수 만들기_nonfin

--- prac40.py
+++ prac40.py
@@ -1,6 +1,16 @@
 def solution(number, k):
     answer = ''
+    numbers = list(map(int, [x for x in number]))
+    numbers_s = sorted(numbers)
+
+    for i in range(k):
+      del numbers[numbers.index(numbers_s[0])]
+      del numbers_s[0]
+
+    for x in numbers:
+      answer += str(x)
     return answer
 
-number = "1924"
-k = 2
+number = "4177252841"
+k = 4
+print(solution(number, k)) #"775841"
