efcd14b3c48d5ff520f3e814f47d9f6ff801085e
jeonhaelee <jhealee0624@gmail.com>
2022-05-18 01:04:11 +0900

    후보키_nonfin

--- prac41.py
+++ prac41.py
@@ -1,5 +1,14 @@
 def solution(relation):
     answer = 0
+    d = []
+    for i in range(len(relation[0])):
+        sub_d = []
+        for row in relation:
+            sub_d.append(row[i])
+        d.append(sub_d)
+        
+    print(d)
+    
     return answer
 
 relation = [["100","ryan","music","2"],["200","apeach","math","2"],
