1f9ac1d8760ca16db0a6b711ef07a95fff312275
jeonhaelee <jhealee0624@gmail.com>
2022-05-25 23:27:02 +0900

    타겟 넘버_nonfin

--- prac44.py
+++ prac44.py
@@ -1,56 +1,47 @@
+# 다시 새롭게 만들어보자.
+
 import math
 
 def solution(numbers, target):
-    answer = 0
-    li = []
+    true_answer = 0
 
-    for n in numbers:
+    answer = numbers[0]
+
+    li = [['+', numbers[0]]]
+    for i in range(1, len(numbers)):
       if answer < target:
-        answer += n
-        li.append(['+', n])
+        answer += numbers[i]
+        li.append(['+', numbers[i]])
       else :
-        answer -= n
-        li.append(['-', n])
+        answer -= numbers[i]
+        li.append(['-', numbers[i]])
     
     print(li)
-    # 같은 수들 중 +, - 다른 것들 순열
-
-    nums = []
-    nums_set = []
-    for n in li:
-      if n in nums:
-        nums_set[nums.index(n)][1] += 1
-      else:
-        nums.append(n)
-        nums_set.append([n,1])
-      
-    print("nums : ",end = "")
-    print(nums)
-    print("nums_set : ",end = "")
-    print(nums_set)
-
-    
-    sub_ans = 1
-    sub_over_one = 0
-    for set in nums_set:
-      if set[1] > 1:
-        sub_over_one += 1
-        sub_ans *= math.factorial(set[1])
+    print(answer)
+    if answer == target:
+      true_answer += 1
 
 
+    answer = -numbers[0]
+    li = [['-', numbers[0]]]
+    for i in range(1, len(numbers)):
+      if answer < target:
+        answer += numbers[i]
+        li.append(['+', numbers[i]])
+      else :
+        answer -= numbers[i]
+        li.append(['-', numbers[i]])
 
-    print("len(li)-sub_over_one : ", end = "")
-    print(len(li)-sub_over_one)
-    print("sub_ans : ",end = "")
-    print(sub_ans)
+    print(li)
+    print(answer)
+    if answer == target:
+      true_answer += 1
 
-    answer = sub_ans
+    return true_answer
 
-    return int(answer)
-
-numbers = [1, 1, 1, 1, 1]
-target = 3
-print(solution(numbers, target)) # 5
+# numbers = [1, 1, 1, 1, 1]
+# target = 3
+# print(solution(numbers, target)) # 5
 
 numbers = [4, 1, 2, 1]
 target = 4
