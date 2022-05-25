15ab29d1dcccd38a8b1becda38545815dfcd5b33
jeonhaelee <jhealee0624@gmail.com>
2022-05-25 23:29:45 +0900

    타겟 넘버_nonfin

--- prac44.py
+++ prac44.py
@@ -2,13 +2,15 @@ import math
 
 def solution(numbers, target):
     true_answer = 0
+    li_answer = []
 
     count = 0
     while count < len(numbers):
       answer = numbers[count]
-      li = [['+', numbers[count]]]
+      li = []
       for i in range(len(numbers)):
         if i == count:
+          li.append(['+', numbers[count]])
           continue
         if answer < target:
           answer += numbers[i]
@@ -19,14 +21,17 @@ def solution(numbers, target):
       
       print(li)
       print(answer)
-      if answer == target:
-        true_answer += 1
+      if li not in li_answer:
+        li_answer.append(li)
+        if answer == target:
+          true_answer += 1
 
 
       answer = -numbers[count]
-      li = [['-', numbers[count]]]
+      li = []
       for i in range(len(numbers)):
         if i == count:
+          li.append(['-', numbers[count]])
           continue
         if answer < target:
           answer += numbers[i]
@@ -37,17 +42,19 @@ def solution(numbers, target):
 
       print(li)
       print(answer)
-      if answer == target:
-        true_answer += 1
+      if li not in li_answer:
+        li_answer.append(li)
+        if answer == target:
+          true_answer += 1
 
       count += 1
 
 
     return true_answer
 
-# numbers = [1, 1, 1, 1, 1]
-# target = 3
-# print(solution(numbers, target)) # 5
+numbers = [1, 1, 1, 1, 1]
+target = 3
+print(solution(numbers, target)) # 5
 
 numbers = [4, 1, 2, 1]
 target = 4
