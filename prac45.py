54d9665dbc28909714b85640ad42c13a60297dfd
jeonhaelee <jhealee0624@gmail.com>
2022-05-27 00:50:41 +0900

    영어 끝말잇기_nonfin

--- prac45.py
+++ prac45.py
@@ -3,10 +3,20 @@ def solution(n, words):
     finish = []
     
     for i in range(len(words)):
+        if i == 0:
+            finish.append(words[i])
+            continue
+        
+        if words[i][0] != words[i-1][-1]:
+            answer.append((i+1)%n)
+            answer.append((i+1)//n)
+            return answer
+        
         if words[i] in finish:
             answer.append((i+1)%n)
             answer.append((i+1)//n)
             return answer
+        
         else:
             finish.append(words[i])
 
@@ -14,4 +24,4 @@ def solution(n, words):
 
 n = 2
 words = ["hello", "one", "even", "never", "now", "world", "draw"]
-print(solution(n, words))
+print(solution(n, words)) # [1, 3]
