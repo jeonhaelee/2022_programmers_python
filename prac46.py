c7525e6443790c742ac784a82802b67f53f1e0af
jeonhaelee <jhealee0624@gmail.com>
2022-06-01 22:34:52 +0900

    괄호 변환_nonfin

--- prac46.py
+++ prac46.py
@@ -1,3 +1,12 @@
+def get_right(w):
+    u = ""; v = ""
+    for i in range(int(len(u)/2)):
+        if u[i] != "(":
+            answer += "("
+            break
+    answer += u
+
+
 def solution(p):
     answer = ""
     rear_answer = ""
@@ -5,18 +14,13 @@ def solution(p):
     if p == "":
         return ""
     
-    u = ""
-    v = ""
+    w = ""
     for i in range(len(p)):
-        if u.count("(") == u. count(")"):
-            v += p[i]
-        else: u += p[i]
-    
-    for i in range(len(u)/2):
-        if u[i] != "(":
-            
-            break
-    answer += u
+        if w.count("(") == w. count(")"):
+            get_right(w)
+            w = ""
+        else: w += p[i]
+
     return answer
 
 p = "(()())()"
