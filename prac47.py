3c67c20243fd2d74f6ca37917b32f667e9a83bef
jeonhaelee <jhealee0624@gmail.com>
2022-06-05 23:43:05 +0900

    2 x n 타일링_nonfin

--- prac47.py
+++ prac47.py
@@ -1,11 +1,14 @@
 # 2 x n 타일링
 # 세로의 길이가 2, 가로의 길이가 n
+import math
 
 def solution(n):
-    answer = 0
-    if n % 2 == 0:
-        
-    else:
+    answer = 1
+    
+    i = 1
+    while 2*i <= n:
+        answer += math.factorial(n-i)/(math.factorial(i)*math.factorial(n-2*i))
+        i += 1
         
         
     return answer
