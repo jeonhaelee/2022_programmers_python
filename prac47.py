921904fb70cf646e672bd1a9a350241099aa2596
jeonhaelee <jhealee0624@gmail.com>
2022-06-06 00:15:28 +0900

    2 x n 타일링_fin

--- prac47.py
+++ prac47.py
@@ -1,17 +1,20 @@
 # 2 x n 타일링
 # 세로의 길이가 2, 가로의 길이가 n
-import math
 
 def solution(n):
-    answer = 1
     
-    i = 1
-    while 2*i <= n:
-        answer += math.factorial(n-i)/(math.factorial(i)*math.factorial(n-2*i))
-        i += 1
-        
-        
-    return int(answer)
+    count1 = 1; count2 = 2
+    if n == 1:
+        return count1
+    elif n == 2:
+        return count2
+    for i in range(n-2):
+        temp=count1
+        count1=count2
+        count2=count1+temp
+    
+    answer = count2%1000000007
+    return answer
 
 n = 4
 print(solution(n))
