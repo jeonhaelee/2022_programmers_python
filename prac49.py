23b9e1398e7d23a9fd0bbaee7c032aa3fc5a0ab1
jeonhaelee <jhealee0624@gmail.com>
2022-06-08 23:43:53 +0900

    로또의 최고 순위와 최저 순위_nonfin

--- prac49.py
+++ prac49.py
@@ -2,14 +2,40 @@
 
 def solution(lottos, win_nums):
     answer = []
+    
     zero = 0; same = 0
     for n in lottos:
         if n == 0:
             zero += 1
         elif n in win_nums:
             same += 1
-    answer.append(same)
-    answer.append(zero)
+            
+    if same == 6:
+        answer.append(1)
+    elif same == 5:
+        answer.append(2)
+    elif same == 4:
+        answer.append(3)
+    elif same == 3:
+        answer.append(4)
+    elif same == 2:
+        answer.append(5)
+    else:
+        answer.append(6)
+        
+    same += zero
+    if same == 6:
+        answer.append(1)
+    elif same == 5:
+        answer.append(2)
+    elif same == 4:
+        answer.append(3)
+    elif same == 3:
+        answer.append(4)
+    elif same == 2:
+        answer.append(5)
+    else:
+        answer.append(6)
     return answer
 
 lottos = [44, 1, 0, 0, 31, 25]
