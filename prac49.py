e664222ce05ba191b0f83525c6648efde7db06e5
jeonhaelee <jhealee0624@gmail.com>
2022-06-08 23:46:26 +0900

    로또의 최고 순위와 최저 순위_fin

--- prac49.py
+++ prac49.py
@@ -1,5 +1,6 @@
 # 로또의 최고 순위와 최저 순위
 
+# 내 풀이
 def solution(lottos, win_nums):
     answer = []
     
@@ -41,3 +42,15 @@ def solution(lottos, win_nums):
 lottos = [44, 1, 0, 0, 31, 25]
 win_nums = [31, 10, 45, 1, 6, 19]
 print(solution(lottos, win_nums)) # [3, 5]
+
+# 다른 사람 풀이
+def solution(lottos, win_nums):
+
+    rank=[6,6,5,4,3,2,1]
+
+    cnt_0 = lottos.count(0)
+    ans = 0
+    for x in win_nums:
+        if x in lottos:
+            ans += 1
+    return rank[cnt_0 + ans],rank[ans]
