ab0c8edae2bcfccb1e225982f76d68e8ad5e80c9
jeonhaelee <jhealee0624@gmail.com>
2022-06-09 23:21:02 +0900

    신고 결과 받기_nonfin

---
+++ prac50.py
@@ -0,0 +1,11 @@
+# 신고 결과 받기
+
+
+def solution(id_list, report, k):
+    answer = []
+    return answer
+
+id_list = ["muzi", "frodo", "apeach", "neo"]
+report = ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"]
+k = 2
+print(solution(id_list, report, k))    # [2,1,1,0]
