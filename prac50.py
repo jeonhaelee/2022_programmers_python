2061285f2a349e7579c8f343cdc81b64a0765e58
jeonhaelee <jhealee0624@gmail.com>
2022-06-10 00:13:44 +0900

    신고 결과 받기_fin

--- prac50.py
+++ prac50.py
@@ -1,5 +1,6 @@
 # 신고 결과 받기
 
+# 내 풀이
 
 def solution(id_list, report, k):
     answer = []; count = []; result = []
@@ -36,3 +37,19 @@ id_list = ["con", "ryan"]
 report = ["ryan con", "ryan con", "ryan con", "ryan con"]
 k = 3
 print(solution(id_list, report, k))    # [0,0]
+
+# 다른 사람 풀이
+# set으로 중복된 것을 지워주고 함...
+
+def solution(id_list, report, k):
+    answer = [0] * len(id_list)    
+    reports = {x : 0 for x in id_list}
+
+    for r in set(report):
+        reports[r.split()[1]] += 1
+
+    for r in set(report):
+        if reports[r.split()[1]] >= k:
+            answer[id_list.index(r.split()[0])] += 1
+
+    return answer
