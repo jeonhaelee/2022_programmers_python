5d97c8dea7e678a8154aa9994fc599eb75049d8a
jeonhaelee <jhealee0624@gmail.com>
2022-06-14 23:59:06 +0900

    실패율_nonfin

--- prac51.py
+++ prac51.py
@@ -2,29 +2,37 @@
 # 스테이지 개수 : N
 # stages 각 자연수는 사용자가 현재 도전 중인 스테이지의 번호를 나타낸다.
 
+# def solution(N, stages):
+#     answer = []
+#     sub_answer = []
+    
+#     sub = []
+#     for i in range(N):
+#         sub.append(0)
+        
+#     for i in range(len(stages)):
+#         for j in range(stages[i]):
+#             sub[j-1] += 1
+    
+#     print(sub)
+#     for i in range(N):
+#         sub_answer.append([(len(stages)-sub[i])/len(stages), i])
+    
+#     sub_answer.sort(reverse=True)
+#     print(sub_answer)
+    
+#     for n in sub_answer:
+#         answer.append(n[1] + 1)
+        
+#     return answer
+
+
 def solution(N, stages):
     answer = []
-    sub_answer = []
     
-    sub = []
-    for i in range(N):
-        sub.append(0)
-        
-    for i in range(len(stages)):
-        for j in range(stages[i]):
-            sub[j-1] += 1
-    
-    print(sub)
-    for i in range(N):
-        sub_answer.append([(len(stages)-sub[i])/len(stages), i])
-    
-    sub_answer.sort(reverse=True)
-    print(sub_answer)
-    
-    for n in sub_answer:
-        answer.append(n[1] + 1)
         
     return answer
+
 
 N = 5
 stages = [2, 1, 2, 6, 2, 4, 3, 3]
