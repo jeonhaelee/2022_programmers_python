69a8c649b6e7b3100dc6796e3cb640882f1551ff
jeonhaelee <jhealee0624@gmail.com>
2022-06-17 16:33:09 +0900

    실패율_fin

--- prac51.py
+++ prac51.py
@@ -24,22 +24,16 @@ def solution(N, stages):
         i += 1
         if i == N + 1:
             break
-
-    print(f'loss : {loss}')
     
     for i in range(N):
-        print(f'{loss[i]}/{remain}')
         if remain == 0:
             ratio = 0
         else:
             ratio = (loss[i]/remain)
-        print(f'{i+1}번째 ratio : {ratio}')
         remain -= loss[i]
         sub.append([ratio, i+1])
     
-    print(f'정렬 전 sub : {sub}')
     sub.sort(key= lambda x : (-x[0], x[1])) 
-    print(f'정렬 후 sub : {sub}')
     
     for i in range(N):
         answer.append(sub[i][1])
@@ -47,13 +41,13 @@ def solution(N, stages):
     return answer
 
 
-# N = 5
-# stages = [2, 1, 2, 6, 2, 4, 3, 3]
-# print(solution(N, stages))  # [3,4,2,1,5]
+N = 5
+stages = [2, 1, 2, 6, 2, 4, 3, 3]
+print(solution(N, stages))  # [3,4,2,1,5]
 
-# N = 4
-# stages = [4,4,4,4,4]
-# print(solution(N, stages))  # [4,1,2,3]
+N = 4
+stages = [4,4,4,4,4]
+print(solution(N, stages))  # [4,1,2,3]
 
 N = 5
 stages = [3, 3, 3, 3]
