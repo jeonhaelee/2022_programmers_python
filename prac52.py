0661c16903180fbae2b8b709eb75e4d98403dbfb
jeonhaelee <jhealee0624@gmail.com>
2022-06-18 03:15:27 +0900

    [1차] 다트 게임_nonfin

---
+++ prac52.py
@@ -0,0 +1,13 @@
+# [1차] 다트 게임
+
+def solution(dartResult):
+    answer = 0
+    return answer
+
+dartResult = "1S2D*3T"
+answer = 37
+print(solution(dartResult)) 
+
+dartResult = "1D2S#10S"
+answer = 9
+print(solution(dartResult)) 
