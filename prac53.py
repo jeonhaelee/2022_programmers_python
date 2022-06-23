3d18b206f0aa0cf4045ec82d920023d7f9d25f81
jeonhaelee <jhealee0624@gmail.com>
2022-06-23 21:46:06 +0900

    비밀지도_nonfin

--- prac53.py
+++ prac53.py
@@ -4,6 +4,26 @@
 
 def solution(n, arr1, arr2):
     answer = []
+    for i in range(n):
+        answer.append([])
+
+    arr1_fin = []
+    for num in arr1:
+        str = bin(num)
+        arr1_fin.append(list(str[2:]))
+    
+    arr2_fin = []
+    for num in arr2:
+        str = bin(num)
+        arr2_fin.append(list(str[2:]))
+    
+    for i in range(n):
+        for j in range(n):
+            if arr1_fin[i][j] == '0' and arr2_fin[i][j] == '0':
+                answer[i][j] = " "
+            else:
+                answer[i][j] = "#"
+                
     return answer
 
 n = 5
@@ -11,7 +31,8 @@ arr1 = [9, 20, 28, 18, 11]
 arr2 = [30, 1, 21, 17, 28]
 print(solution(n, arr1, arr2)) # ["#####","# # #", "### #", "# ##", "#####"]
 
-n = 6
-arr1 = [46, 33, 33 ,22, 31, 50]
-arr2 = [27 ,56, 19, 14, 14, 10]
-print(solution(n, arr1, arr2)) # ["######", "### #", "## ##", " #### ", " #####", "### # "]
+# n = 6
+# arr1 = [46, 33, 33 ,22, 31, 50]
+# arr2 = [27 ,56, 19, 14, 14, 10]
+# print(solution(n, arr1, arr2)) # ["######", "### #", "## ##", " #### ", " #####", "### # "]
+
