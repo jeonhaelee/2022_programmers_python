466ddd9171af812b9b9cb89e45537d638882c71a
jeonhaelee <jhealee0624@gmail.com>
2022-06-23 22:30:45 +0900

    비밀지도_nonfin

--- prac53.py
+++ prac53.py
@@ -2,12 +2,10 @@
 # 지도는 한 변의 길이가 n인 정사각형 배열 형태로, 
 # 각 칸은 "공백"(" ") 또는 "벽"("#") 두 종류로 이루어져 있다.
 
+
 def solution(n, arr1, arr2):
     answer = []
-    for i in range(n):
-        for j in range(n):
-            answer.append([0])
-
+        
     arr1_fin = []
     for num in arr1:
         str = list(bin(num)[2:])
@@ -21,16 +19,15 @@ def solution(n, arr1, arr2):
         while len(str) != n:
             str.insert(0, '0')
         arr2_fin.append(str)
-    
-    print(arr1_fin)
-    print(arr2_fin)
 
     for i in range(n):
+        sub_answer = ""
         for j in range(n):
             if arr1_fin[i][j] == '0' and arr2_fin[i][j] == '0':
-                answer[i][j] = " "
+                sub_answer += " "
             else:
-                answer[i][j] = "#"
+                sub_answer += "#"
+        answer.append(sub_answer)
                 
     return answer
 
