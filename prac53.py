3a25eaccbb7126d5b4bf7d1e84792c9a115d38a5
jeonhaelee <jhealee0624@gmail.com>
2022-06-23 22:11:34 +0900

    비밀지도_nonfin

--- prac53.py
+++ prac53.py
@@ -9,14 +9,20 @@ def solution(n, arr1, arr2):
 
     arr1_fin = []
     for num in arr1:
-        str = bin(num)
-        arr1_fin.append(list(str[2:]))
+        str = list(bin(num)[2:])
+        while len(str) != n:
+            str.insert(0, '0')
+        arr1_fin.append(str)
     
     arr2_fin = []
     for num in arr2:
-        str = bin(num)
-        arr2_fin.append(list(str[2:]))
+        str = list(bin(num)[2:])
+        while len(str) != n:
+            str.insert(0, '0')
+        arr1_fin.append(str)
     
+    # 만약 숫자의 길이가 5가 아닐경우가 있으니,
+    # 일단 n * n answer을 만들어주고 0을 채워넣자.
 
     for i in range(n):
         for j in range(n):
