2ab59e47b4204b9f846375692685fe0fbc8a1a1b
jeonhaelee <jhealee0624@gmail.com>
2022-06-26 23:59:29 +0900

    크레인 인형뽑기 게임_nonfin

--- prac56.py
+++ prac56.py
@@ -1,5 +1,10 @@
 # 크레인 인형뽑기 게임
+# "N x N" 크기의 정사각 격자
 
 def solution(board, moves):
     answer = 0
     return answer
+
+board = [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]
+moves = [1,5,3,5,1,2,1,4]
+print(solution(board, moves)) # 4
