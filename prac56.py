53d3733581ba3cd60c497391c9aa86ed4ec05811
jeonhaelee <jhealee0624@gmail.com>
2022-06-27 23:41:45 +0900

    크레인 인형뽑기 게임_nonfin

--- prac56.py
+++ prac56.py
@@ -11,13 +11,16 @@ def solution(board, moves):
         num = moves[i]
         
         get.append(board[num][-1])
-        del board[num][-1]
+        board[num][-1] = 0
         
         if len(get) >= 2 and get[-2] == get[-1]:
             del get[-1]
             del get[-1]
             answer += 1
             
+    print(board) 
+    print(get)
+    
     return answer
 
 board = [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]
