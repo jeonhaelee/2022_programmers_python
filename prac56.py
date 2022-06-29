baed5e0924acecf63ef67c9ff5b0b5846bc2307e
jeonhaelee <jhealee0624@gmail.com>
2022-06-30 00:17:50 +0900

    크레인 인형뽑기 게임_nonfin

--- prac56.py
+++ prac56.py
@@ -126,3 +126,39 @@ def solution(board, moves):
 board = [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]
 moves = [1,5,3,5,1,2,1,4]
 print(solution(board, moves)) # 4
+
+# 다른 사람 풀이 1
+
+def solution(board, moves):
+    stacklist = []
+    answer = 0
+
+    for i in moves:
+        for j in range(len(board)):
+            if board[j][i-1] != 0:
+                stacklist.append(board[j][i-1])
+                board[j][i-1] = 0
+
+                if len(stacklist) > 1:
+                    if stacklist[-1] == stacklist[-2]:
+                        stacklist.pop(-1)
+                        stacklist.pop(-1)
+                        answer += 2     
+                break
+
+    return answer
+
+# 다른 사람 풀이 2
+
+def solution(board, moves):
+    cols = list(map(lambda x: list(filter(lambda y: y > 0, x)), zip(*board)))
+    a, s = 0, [0]
+
+    for m in moves:
+        if len(cols[m - 1]) > 0:
+            if (d := cols[m - 1].pop(0)) == (l := s.pop()):
+                a += 2
+            else:
+                s.extend([l, d])
+
+    return a
