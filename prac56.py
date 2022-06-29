c434a5db14284be49f9eaa7f80ae7d3b66661381
jeonhaelee <jhealee0624@gmail.com>
2022-06-29 23:52:41 +0900

    크레인 인형뽑기 게임_nonfin

--- prac56.py
+++ prac56.py
@@ -52,7 +52,7 @@ def solution(board, moves):
     
     print(board)
     print(get)
-    return answer
+    return answer*2
 
 board = [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]
 moves = [1,5,3,5,1,2,1,4]
