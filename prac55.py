1f977604a605ad77b8081be1234817bf88227d09
jeonhaelee <jhealee0624@gmail.com>
2022-07-03 23:48:19 +0900

    키패드 누르기_nonfin

--- prac55.py
+++ prac55.py
@@ -17,7 +17,7 @@ def solution(numbers, hand):
             
         else:
             if num == 2:
-            
+                
             elif num == 5:
                 
             elif num == 8:
@@ -25,3 +25,7 @@ def solution(numbers, hand):
             elif num == 0:
                 
     return answer
+
+numbers = [1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5]
+hand = "right"
+print(solution(numbers, hand))
