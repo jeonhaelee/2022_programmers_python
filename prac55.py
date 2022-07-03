ef50efc9b38accc83537fe33c7e4c642dfc75ff5
jeonhaelee <jhealee0624@gmail.com>
2022-07-04 00:34:50 +0900

    키패드 누르기_nonfin

--- prac55.py
+++ prac55.py
@@ -204,6 +204,17 @@ def solution(numbers, hand):
                 
     return answer
 
-numbers = [1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5]
+
+# numbers = [1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5]
+# hand = "right"
+# print(solution(numbers, hand)) # "LRLLLRLLRRL"
+
+
+# numbers = [7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2]
+# hand = "left"
+# print(solution(numbers, hand)) # "LRLLRRLLLRR"
+
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 hand = "right"
-print(solution(numbers, hand)) # "LRLLLRLLRRL"
+print(solution(numbers, hand)) # "LLRLLRLLRL"
