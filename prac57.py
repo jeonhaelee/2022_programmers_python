82b01e364df25c32a4583fe811d92190be98bc40
jeonhaelee <jhealee0624@gmail.com>
2022-07-05 00:08:04 +0900

    구명보트_fin

--- prac57.py
+++ prac57.py
@@ -1,23 +1,5 @@
 # 구명보트
 
-# def solution(people, limit):
-#     answer = 0
-#     people.sort(reverse = True)
-    
-#     while len(people) > 1:
-#         if people[0] + people[-1] > limit:
-#             answer += 1
-#             del people[0]
-#         else:
-#             answer += 1
-#             del people[0]
-#             del people[-1]
-            
-#     if len(people) == 1:
-#         answer += 1
-#         del people[0]
-        
-#     return answer
 
 def solution(people, limit):
     answer = 0
