88e89a0a2a63455bc1b48460b29a6f179bddca93
jeonhaelee <jhealee0624@gmail.com>
2022-07-05 00:10:58 +0900

    구명보트_fin

--- prac57.py
+++ prac57.py
@@ -1,5 +1,6 @@
 # 구명보트
 
+# 내 풀이
 
 def solution(people, limit):
     answer = 0
@@ -31,3 +32,36 @@ def solution(people, limit):
 people = [70, 80, 50]
 limit = 100
 print(solution(people, limit)) # 3
+
+# 다른 사람 풀이 1
+
+def solution(people, limit) :
+    answer = 0
+    people.sort()
+
+    a = 0
+    b = len(people) - 1
+    while a < b :
+        if people[b] + people[a] <= limit :
+            a += 1
+            answer += 1
+        b -= 1
+    return len(people) - answer
+
+# 다른 사람 풀이 2
+
+from collections import deque
+
+def solution(people, limit):
+    result = 0
+    deque_people = deque(sorted(people))
+
+    while deque_people:
+        left = deque_people.popleft()
+        if not deque_people:
+            return result + 1
+        right = deque_people.pop()
+        if left + right > limit:
+            deque_people.appendleft(left)
+        result += 1
+    return result
