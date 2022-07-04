ea1d96fc09714e0c3dd19e90b37ab399bf466ed7
jeonhaelee <jhealee0624@gmail.com>
2022-07-05 00:19:21 +0900

    메뉴 리뉴얼_nonfin

---
+++ prac58.py
@@ -0,0 +1,9 @@
+# 메뉴 리뉴얼
+
+def solution(orders, course):
+    answer = []
+    return answer
+
+orders = ["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"]
+course = [2,3,4]
+print(solution(orders, course)) # ["AC", "ACDE", "BCFG", "CDE"]
