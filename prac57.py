b463494e2d64df38f8d1c36b48b2defb11a62a1d
jeonhaelee <jhealee0624@gmail.com>
2022-07-05 00:06:57 +0900

    구명보트_nonfin

--- prac57.py
+++ prac57.py
@@ -26,7 +26,11 @@ def solution(people, limit):
     l_idx = 0
     r_idx = len(people)-1
     
-    while r_idx - l_idx > 0:
+    while r_idx - l_idx >= 0:
+        if r_idx == l_idx :
+            answer += 1
+            break
+        
         if people[l_idx] + people[r_idx] > limit:
             answer += 1
             l_idx += 1
@@ -35,12 +39,13 @@ def solution(people, limit):
             l_idx += 1
             r_idx -= 1
             
-    if len(people) == 1:
-        answer += 1
-        
     return answer
 
 
-people = [70, 50, 80, 50]
+# people = [70, 50, 80, 50]
+# limit = 100
+# print(solution(people, limit)) # 3
+
+people = [70, 80, 50]
 limit = 100
 print(solution(people, limit)) # 3
