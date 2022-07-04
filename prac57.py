9d472a1ca70c72c5562f31566a269fc777a2d981
jeonhaelee <jhealee0624@gmail.com>
2022-07-04 23:50:31 +0900

    구명보트_nonfin

--- prac57.py
+++ prac57.py
@@ -3,7 +3,16 @@
 def solution(people, limit):
     answer = 0
     people.sort(reverse = True)
-    print(people)
+    
+    while len(people) > 1:
+        if people[0] + people[-1] > limit:
+            answer += 1
+            del people[0]
+            
+    if len(people) == 1:
+        answer += 1
+        del people[0]
+        
     return answer
 
 people = [70, 50, 80, 50]
