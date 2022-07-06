4459c275686ed46ee316edba5768b7f8c07ad483
jeonhaelee <jhealee0624@gmail.com>
2022-07-06 22:38:01 +0900

    메뉴 리뉴얼_nonfin

--- prac58.py
+++ prac58.py
@@ -1,7 +1,20 @@
 # 메뉴 리뉴얼
 
+def compare_menu(i, orders):
+    target = orders[i]
+    
+    for order in range(i+1, len(orders)):
+        if order[:len(target)] == target:
+            return True
+        
 def solution(orders, course):
     answer = []
+    orders.sort(key = lambda x : len(x))
+    
+    for i, order in enumerate(orders):
+        if compare_menu(i, orders):
+            answer.append(orders[i])
+
     return answer
 
 orders = ["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"]
