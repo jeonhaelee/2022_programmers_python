1328b6fcefbbcaffbf9ef595903dcf838fc26d98
jeonhaelee <jhealee0624@gmail.com>
2022-07-09 23:15:06 +0900

    메뉴 리뉴얼_nonfin

--- prac58.py
+++ prac58.py
@@ -66,18 +66,18 @@ def solution(orders, course):
             
     print(f'orders : {orders}')
     
-    # for i, order in enumerate(orders):
+    for i, order in enumerate(orders):
         
-    #     if order in answer:
-    #         count_answer[answer.index(order)] += 1
-    #         continue
+        if order in answer:
+            count_answer[answer.index(order)] += 1
+            continue
             
-    #     if len(orders[i]) not in course:
-    #         continue
+        if len(orders[i]) not in course:
+            continue
 
-    #     if compare_menu(i, orders):
-    #         answer.append(orders)
-    #         count_answer.append(1)
+        if compare_menu(i, orders):
+            answer.append(orders)
+            count_answer.append(1)
 
     print(f'answer : {answer}')
     print(f'count_answer : {count_answer}')
