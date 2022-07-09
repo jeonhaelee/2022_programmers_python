fe9f4c947717e5bff7bef256b74bf77af56699f0
jeonhaelee <jhealee0624@gmail.com>
2022-07-09 23:11:47 +0900

    메뉴 리뉴얼_nonfin

--- prac58.py
+++ prac58.py
@@ -101,13 +101,13 @@ def solution(orders, course):
         
     return real_answer
 
-# orders = ["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"]
-# course = [2,3,4]
-# print(solution(orders, course)) # ["AC", "ACDE", "BCFG", "CDE"]
+orders = ["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"]
+course = [2,3,4]
+print(solution(orders, course)) # ["AC", "ACDE", "BCFG", "CDE"]
 
-orders = ["ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"]
-course = [2,3,5]
-print(solution(orders, course)) # ["ACD", "AD", "ADE", "CD", "XYZ"]
+# orders = ["ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"]
+# course = [2,3,5]
+# print(solution(orders, course)) # ["ACD", "AD", "ADE", "CD", "XYZ"]
 
 # orders = ["XYZ", "XWY", "WXA"]
 # course = [2,3,4]
