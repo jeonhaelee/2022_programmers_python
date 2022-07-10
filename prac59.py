76ae84713f26756d1899a86dedcd6d151b401bdb
jeonhaelee <jhealee0624@gmail.com>
2022-07-10 23:59:15 +0900

    큰 수 만들기_nonfin

--- prac59.py
+++ prac59.py
@@ -11,3 +11,11 @@ def solution(number, k):
 number = "1924"
 k = 2
 print(solution(number, k)) # "94"
+
+number = "1231234"
+k = 3
+print(solution(number, k)) # "3234"
+
+number = "4177252841"
+k = 4
+print(solution(number, k)) # "775841"
