5b550ece783784bebcf4bd761566f5bf12f3527f
jeonhaelee <jhealee0624@gmail.com>
2022-07-15 22:39:47 +0900

    거리두기 확인하기_nonfin

--- prac60.py
+++ prac60.py
@@ -1,10 +1,20 @@
 # 거리두기 확인하기
 
-def solution(places):
-    answer = []
-    return answer
+# def solution(places):
+#     answer = []
+#     return answer
 
-places = [["POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"], ["POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"], 
-          ["PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"], ["OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"], 
-          ["PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"]]
-print(solution(places)) # [1, 0, 1, 1, 1]
+# places = [["POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"], ["POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"], 
+#           ["PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"], ["OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"], 
+#           ["PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"]]
+# print(solution(places)) # [1, 0, 1, 1, 1]
+
+
+place = ["POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"]
+
+place_list = []
+
+for i in range(len(place)):
+    place_list.append((list(place[i])))
+    
+print(place_list)
