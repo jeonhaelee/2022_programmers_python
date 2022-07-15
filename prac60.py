7bd9f85f7246dab08d767783d46ae0b5fd13acf0
jeonhaelee <jhealee0624@gmail.com>
2022-07-15 23:31:49 +0900

    거리두기 확인하기_nonfin

--- prac60.py
+++ prac60.py
@@ -18,3 +18,84 @@ for i in range(len(place)):
     place_list.append((list(place[i])))
     
 print(place_list)
+
+
+    
+def check_place(place):
+    for i in range(5):
+        for j in range(5):
+            
+            if i<0 or i>4 or j<0 or j>4:
+                pass
+            else:
+                if 'P' == place[i-1][j]:
+                    return 0
+            if i<0 or i>4 or j<0 or j>4:
+                pass
+            else:
+                if 'P' == place[i][j-1]:
+                    return 0
+            if i<0 or i>4 or j<0 or j>4:
+                pass
+            else:
+                if 'P' == place[i+1][j]:
+                    return 0
+            if i<0 or i>4 or j<0 or j>4:
+                pass
+            else:
+                if 'P' == place[i][j+1]:
+                    return 0
+            
+            if i<0 or i>4 or j<0 or j>4:
+                pass
+            else:
+                if 'P' == place[i][j-2]:
+                    if 'X' != place[i][j-1]:
+                        return 0
+            if i<0 or i>4 or j<0 or j>4:
+                pass
+            else:
+                if 'P' == place[i][j+2]:
+                    if 'X' != place[i][j+1]:
+                        return 0
+            if i<0 or i>4 or j<0 or j>4:
+                pass
+            else:
+                if 'P' == place[i-2][j]:
+                    if 'X' != place[i-1][j]:
+                        return 0
+            if i<0 or i>4 or j<0 or j>4:
+                pass
+            else:
+                if 'P' == place[i+2][j]:
+                    if 'X' != place[i+1][j]:
+                        return 0
+            
+            if i<0 or i>4 or j<0 or j>4:
+                pass
+            else:
+                if 'P' == place[i-1][j-1]:
+                    if 'X' != place[i][j-1] or 'X' != place[i-1][j]:
+                        return 0
+            if i<0 or i>4 or j<0 or j>4:
+                pass
+            else:
+                if 'P' == place[i-1][j+1]:
+                    if 'X' != place[i][j+1] or 'X' != place[i-1][j]:
+                        return 0
+            if i<0 or i>4 or j<0 or j>4:
+                pass
+            else:
+                if 'P' == place[i+1][j-1]:
+                    if 'X' != place[i][j-1] or 'X' != place[i+1][j]:
+                        return 0
+            if i<0 or i>4 or j<0 or j>4:
+                pass
+            else:
+                if 'P' == place[i+1][j+1]:
+                    if 'X' != place[i][j+1] or 'X' != place[i][j+1]:
+                        return 0
+            
+    return 1
+
+print(check_place(place))
