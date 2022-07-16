2f3be49aac8b598fd97b8f22744d6d2c75742e49
jeonhaelee <jhealee0624@gmail.com>
2022-07-16 23:54:03 +0900

    거리두기 확인하기_nonfin

--- prac60.py
+++ prac60.py
@@ -6,10 +6,22 @@
 # 오른쪽에 X, 아래쪽에 X가 있으면 오른쪽아래대각선은 생각 안 해도 되니까.
 # 이것들 중 X가 없을 경우에만 한칸 더 가서 P인지 아닌지만 따져주면 될 듯!!!
 
+place = ["POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"]
 
+def check_place(place):
+    place_list = []
+
+    for i in range(len(place)):
+        place_list.append((list(place[i])))
+        
+    return 1
 
 def solution(places):
     answer = []
+    
+    for place in places:
+        answer.append(check_place(place))
+        
     return answer
 
 places = [["POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"], ["POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"], 
