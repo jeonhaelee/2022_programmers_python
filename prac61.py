874ee8ee1a02470ed1761594983e1c511d9ab469
jeonhaelee <jhealee0624@gmail.com>
2022-07-20 03:08:21 +0900

    문자열 압축_nonfin

---
+++ prac61.py
@@ -0,0 +1,20 @@
+# 문자열 압축
+
+def solution(s):
+    answer = 0
+    return answer
+
+s = "aabbaccc"
+print(solution(s)) # 7
+
+s = "ababcdcdababcdcd"
+print(solution(s)) # 9
+
+s = "abcabcdede"
+print(solution(s)) # 8
+
+s = "abcabcabcabcdededededede"
+print(solution(s)) # 14
+
+s = "xababcdcdababcdcd"
+print(solution(s)) # 17
