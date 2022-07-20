158f01acb4b09cc3c1b85cb632e9db50b16f97a8
jeonhaelee <jhealee0624@gmail.com>
2022-07-21 00:53:34 +0900

    오픈채팅방_fin

--- prac62.py
+++ prac62.py
@@ -1,49 +1,8 @@
 # 오픈채팅방
-# 시간초과 해결 요함. 딕셔너리 이용해보자.
 
-
-# def solution(record):
-#     answer = []
-#     user_id = []
-#     user_name = []
-    
-#     for re in record:
-#         action, info = re.split(maxsplit=1)
-        
-#         if action == 'Enter':
-#             id, name = info.split()
-#             if id not in user_id:
-#                 user_id.append(id)
-#                 user_name.append(name)
-#             else:
-#                 idx = user_id.index(id)
-#                 user_name[idx] = name
-        
-#         if action == 'Change':
-#             id, name = info.split()
-#             idx = user_id.index(id)
-#             user_name[idx] = name
-        
-#     for re in record:
-#         action, info = re.split(maxsplit=1)
-    
-#         if action == 'Enter':
-#             id, name = info.split()
-#             idx = user_id.index(id)
-#             sentence = user_name[idx] + "님이 들어왔습니다."
-#             answer.append(sentence)
-            
-#         if action == 'Leave':
-#             id = info
-#             idx = user_id.index(id)
-#             sentence = user_name[idx] + "님이 나갔습니다."
-#             answer.append(sentence)
-            
-#     return answer
-
+# 내 풀이
 def solution(record):
     answer = []
-    user_id = []
     user_info = {}
     
     for re in record:
@@ -51,11 +10,7 @@ def solution(record):
         
         if action == 'Enter':
             id, name = info.split()
-            if id not in user_id:
-                user_id.append(id)
-                user_info[id] = name
-            else:
-                user_info[id] = name
+            user_info[id] = name
         
         if action == 'Change':
             id, name = info.split()
@@ -79,3 +34,19 @@ def solution(record):
 record = ["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"]
 print(solution(record))
 # ["Prodo님이 들어왔습니다.", "Ryan님이 들어왔습니다.", "Prodo님이 나갔습니다.", "Prodo님이 들어왔습니다."]
+
+# 다른 사람 풀이
+def solution(record):
+    answer = []
+    namespace = {}
+    printer = {'Enter':'님이 들어왔습니다.', 'Leave':'님이 나갔습니다.'}
+    for r in record:
+        rr = r.split(' ')
+        if rr[0] in ['Enter', 'Change']:
+            namespace[rr[1]] = rr[2]
+
+    for r in record:
+        if r.split(' ')[0] != 'Change':
+            answer.append(namespace[r.split(' ')[1]] + printer[r.split(' ')[0]])
+
+    return answer
