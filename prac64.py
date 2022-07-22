f9a3082da090568e4c7ae93c7ac2db5fa3e6a591
jeonhaelee <jhealee0624@gmail.com>
2022-07-23 01:08:13 +0900

    캐시_nonfin

---
+++ prac64.py
@@ -0,0 +1,9 @@
+# 캐시
+
+def solution(cacheSize, cities):
+    answer = 0
+    return answer
+
+cacheSize = 3
+cities = ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"]
+print(solution(cacheSize, cities)) # 50
