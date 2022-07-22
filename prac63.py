468a8a90c9c2bbead55fb4df9be237f53206ee06
jeonhaelee <jhealee0624@gmail.com>
2022-07-22 23:44:32 +0900

    뉴스 클러스터링_nonfin

--- prac63.py
+++ prac63.py
@@ -2,6 +2,19 @@
 
 def solution(str1, str2):
     answer = 0
+    str1_list = []; str2_list = []
+    
+    for i in range(len(str1)-1):
+        str = str1[i:i+2]
+        str1_list.append(str)
+    
+    for i in range(len(str2)-1):
+        str = str2[i:i+2]
+        str2_list.append(str)
+    
+    print(str1_list)
+    print(str2_list)
+    
     return answer
 
 str1 = 'FRANCE'
