b3e6468836f9fcb830532d5ad1659c6f98ba5d95
jeonhaelee <jhealee0624@gmail.com>
2022-07-26 23:35:59 +0900

    N개의 최소공배수_nonfin

--- prac67.py
+++ prac67.py
@@ -1,7 +1,22 @@
 # N개의 최소공배수
 
+import math
+
 def solution(arr):
     answer = 0
+    max_num = max(arr)
+    
+    i = max_num
+    while True:
+        count = 0
+        for n in arr:
+            if i % n != 0:
+                count += 1
+        if count > 0:
+            i *= 2
+        else:
+            answer = i
+            break
     return answer
 
 arr = [2,6,8,14]
