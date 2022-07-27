676eb6476fa7e17338eaf3ff0f1e84d140f1366b
jeonhaelee <jhealee0624@gmail.com>
2022-07-27 22:21:48 +0900

    캐시_nonfin

--- prac64.py
+++ prac64.py
@@ -1,7 +1,29 @@
 # 캐시
+# cache hit : 1, cache miss : 5
+
 
 def solution(cacheSize, cities):
     answer = 0
+    
+    if cacheSize == 0:
+        answer = len(cities) * 5
+        return answer
+    
+    pocket = []
+    
+    for city in cities:
+        if city in pocket:
+            del pocket[pocket.index(city)]
+            pocket.append(city)
+            answer += 1
+        else:
+            if len(pocket) < 3:
+                pocket.append(city)
+            else:
+                del pocket[0]
+                pocket.append(city)
+            answer += 5
+            
     return answer
 
 cacheSize = 3
