d9113bc7a3929a5dfb0094842a421a0e50801187
jeonhaelee <jhealee0624@gmail.com>
2022-07-30 18:23:08 +0900

    수식 최대화_nonfin

--- prac68.py
+++ prac68.py
@@ -1,7 +1,27 @@
 # 수식 최대화
+# +, -, *
+
+from itertools import permutations
 
 def solution(expression):
     answer = 0
+    numbers = []; cal = []
+    num = ""
+    for i in range(len(expression)):
+        if expression[i].isdigit() :
+            num += expression[i]
+        else:
+            cal.append(expression[i])
+            numbers.append(int(num))
+            num = ""
+    
+    cals = []
+    cals.append(list(permutations(set(cal))))
+    
+    print(cals)
+    
+    # cals
+    
     return answer
 
 expression = "100-200*300-500+20"
