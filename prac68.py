44b3dd47d568eb05c4ac7d6aa22f3bb16adb9f92
jeonhaelee <jhealee0624@gmail.com>
2022-07-30 18:30:17 +0900

    수식 최대화_nonfin

--- prac68.py
+++ prac68.py
@@ -1,10 +1,12 @@
 # 수식 최대화
 # +, -, *
+# 아니지. 우선순위 높은게 뒤쪽에 나올 경우도 생각하면 앞 뒤 숫자를 다 생각해야지.
+
 
 from itertools import permutations
 
 def solution(expression):
-    answer = 0
+
     numbers = []; cal = []
     num = ""
     for i in range(len(expression)):
@@ -12,15 +14,41 @@ def solution(expression):
             num += expression[i]
         else:
             cal.append(expression[i])
-            numbers.append(int(num))
+            numbers.append(num)
             num = ""
     
-    cals = []
-    cals.append(list(permutations(set(cal))))
+    num_cal = []
+    num_cal.append(numbers[0])
     
-    print(cals)
+    for i in range(1, len(numbers)):
+        num_cal.append(cal[i] + numbers[i])
     
-    # cals
+    cals_permutations = []
+    cals_permutations.append(list(permutations(set(cal))))
+    
+    print(cals_permutations)
+    
+    # cals 돌면서 cal 우선순위대로 숫자 계산해서 가장 높은 숫자 리턴
+    
+    # 만약 우선순위 순서가 -, +, *라면
+    
+    results = []
+    result = int(num_cal[0])
+    for i in range(1, len(num_cal)):
+        if num_cal[i][0] == '-':
+            result -= int(num_cal[i][1:])
+    
+    for i in range(1, len(num_cal)):
+        if num_cal[i][0] == '+':
+            result += int(num_cal[i][1:])
+            
+    for i in range(1, len(num_cal)):
+        if num_cal[i][0] == '*':
+            result *= int(num_cal[i][1:])
+            
+    results.append(abs(result))
+    
+    answer = max(results)
     
     return answer
 
