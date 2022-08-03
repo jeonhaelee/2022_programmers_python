2966b0b592797591698efed2c43dd399a7506cd3
jeonhaelee <jhealee0624@gmail.com>
2022-08-03 23:53:43 +0900

    타겟넘버_nonfin

--- prac44.py
+++ prac44.py
@@ -8,6 +8,18 @@ import math
 def solution(numbers, target):
     answer = 0
 
+    idx = 0
+    result = 0
+    while idx != len(numbers)-1:
+        if result < target:
+            result += numbers[idx]
+            idx += 1
+        else:
+            result -= numbers[idx]
+            idx += 1
+
+    print(result)
+    
     return answer
 
 numbers = [1, 1, 1, 1, 1]
