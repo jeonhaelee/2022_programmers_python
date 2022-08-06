697207c0fdee7bafab2047a1473db9da303246de
jeonhaelee <jhealee0624@gmail.com>
2022-08-06 23:59:53 +0900

    점프와 순간이동_nonfin

--- prac70.py
+++ prac70.py
@@ -1,44 +1,44 @@
 # 점프와 순간이동
 
-counts = []
+import sys
+sys.setrecursionlimit(10**7)
 
-def go_2multi(now, n, count):
+counts = []
+target = 0
+
+def go_2multi(now, count):
     global counts 
+    global tarket
     
     while True:
         get = now * 2
-        go_1(get, n, count)
-        if get > n:
+        go_2multi(get + 1, count + 1)
+        if get > target:
             sub_count = (n-now)
             counts.append(count + sub_count)
             break
-        elif get == n:
+        elif get == target:
             counts.append(count)
             break
         else: now = get
     
 
-def go_1(get, n, count):
-    go_2multi(get + 1, n, count + 1)
-    
-
 def solution(n):
     global counts
+    global target
+    target = n
     
     ans = 0
     now = 0
-    
-    
     count = 0
-    counts = []
-    for i in range(n):
+    for i in range(target):
         now = i
         
         if now == 0:
             count += 1
             continue
         
-        go_2multi(now, n, count)
+        go_2multi(now, count)
         count += 1
     
     counts.append(count)
@@ -48,8 +48,8 @@ def solution(n):
     ans = min(counts)
     return ans
 
-n = 5
-print(solution(n)) # 2
+# n = 5
+# print(solution(n)) # 2
 
 n = 6
 print(solution(n)) # 2
