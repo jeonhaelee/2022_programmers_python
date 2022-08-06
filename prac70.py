268485922ebcc774b806cfa22527dc8457dcbce0
jeonhaelee <jhealee0624@gmail.com>
2022-08-07 01:11:14 +0900

    점프와 순간이동_nonfin

--- prac70.py
+++ prac70.py
@@ -1,55 +1,28 @@
 # 점프와 순간이동
 
-import sys
-sys.setrecursionlimit(10**7)
-
-counts = []
-target = 0
-
-def go_2multi(now, count):
-    global counts 
-    global tarket
-    
-    while True:
-        get = now * 2
-        go_2multi(get + 1, count + 1)
-        if get > target:
-            sub_count = (n-now)
-            counts.append(count + sub_count)
-            break
-        elif get == target:
+def get_twice(num, count, n, counts):
+        if num * 2 > n:
+            counts.append(count + n - num)
+        elif num * 2 == n:
             counts.append(count)
-            break
-        else: now = get
+        else:
+            num *= 2
+            get_twice(num, count, n, counts)
+            get_twice(num + 1, count + 1, n, counts)
     
 
 def solution(n):
-    global counts
-    global target
-    target = n
+    counts = []
     
-    ans = 0
-    now = 0
-    count = 0
-    for i in range(target):
-        now = i
-        
-        if now == 0:
-            count += 1
-            continue
-        
-        go_2multi(now, count)
-        count += 1
+    num = 1; count = 1
+    get_twice(num, count, n, counts)
     
-    counts.append(count)
-    
-    print(counts)
-
     ans = min(counts)
+    
     return ans
 
-# n = 5
-# print(solution(n)) # 2
+n = 5
+print(solution(n)) # 2
 
 n = 6
 print(solution(n)) # 2
