974a1740659b4c7d267295e0ec9b358b8395e7a8
jeonhaelee <jhealee0624@gmail.com>
2022-08-08 23:21:41 +0900

    점프와 순간이동_nonfin

--- prac70.py
+++ prac70.py
@@ -1,32 +1,10 @@
 # 점프와 순간이동
 # 효율성 테스트 다시.
-# 도착지점에서 0으로 돌아오는 법으로 생각해보자...
-# 홀수와 짝수랑 관계가 있나?
-
-def get_twice(num, count, n, counts):
-    
-    while True:
-        if num * 2 > n:
-            counts.append(count + n - num)
-            break
-        elif num * 2 == n:
-            counts.append(count)
-            break
-        else:
-            num *= 2
-            get_twice(num, count, n, counts)
-            get_twice(num + 1, count + 1, n, counts)
-    
 
 def solution(n):
-    counts = []
+    count = 0
     
-    num = 1; count = 1
-    get_twice(num, count, n, counts)
-    
-    ans = min(counts)
-    
-    return ans
+    return count
 
 n = 5
 print(solution(n)) # 2
