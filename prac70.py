932cde753727fbe3577614b59b855638458cf1b5
jeonhaelee <jhealee0624@gmail.com>
2022-08-08 23:24:44 +0900

    점프와 순간이동_fin

--- prac70.py
+++ prac70.py
@@ -1,6 +1,6 @@
 # 점프와 순간이동
-# 효율성 테스트 다시.
 
+# 내 풀이
 def solution(n):
     count = 0
     while n != 0 :
@@ -14,3 +14,16 @@ print(solution(n)) # 2
 
 n = 6
 print(solution(n)) # 2
+
+# 다른 사람 풀이 1
+# -> 이진법을 이용한 풀이
+def solution(n):
+    return bin(n).count('1')
+
+# 다른 사람 풀이 2
+def solution(n):
+    answer = 1
+    while n > 1:
+        answer += n % 2
+        n = n // 2
+    return answer
