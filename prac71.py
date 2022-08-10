ef4c29b608292f7e76ebc17486975c0b4dd78efd
jeonhaelee <jhealee0624@gmail.com>
2022-08-10 23:52:55 +0900

    행렬 테두리 회전하기_nonfin

---
+++ prac71.py
@@ -0,0 +1,10 @@
+# 행렬 테두리 회전하기
+
+def solution(rows, columns, queries):
+    answer = []
+    return answer
+
+rows = 6
+columns = 6
+queries = [[2,2,5,4],[3,3,6,6],[5,1,6,3]]
+print(solution(rows, columns, queries)) # [8, 10, 25]
