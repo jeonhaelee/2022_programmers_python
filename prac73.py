8eacea047a298eb53c42b6b3fd46063333ee8575
jeonhaelee <jhealee0624@gmail.com>
2022-08-17 21:56:50 +0900

    삼각 달팽이_nonfin

--- prac73.py
+++ prac73.py
@@ -3,16 +3,25 @@
 
 def solution(n):
     answer = []
-    num_count = sum(i for i in range(1, n+1))
-    print(num_count)
+    num = 1
     
-    li = []
-
+    li = [[0] * n for i in range(n)]
+    x = -1; y = 0
+    
     for i in range(n):
-        li.append([i+1])
+        for j in range(i, n):
+            if i % 3 == 0:
+                x += 1
+            elif i % 3 == 1:
+                y += 1
+            elif i % 3 == 2:
+                x -= 1
+                y -= 1
+            li[x][y] = num
+            num += 1
     
-
-    print(li)
+    for i in range(n):
+        print(li[i])
     
     return answer
 
