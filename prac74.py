c423df0612f2406c8b0afc4120c3e146b9a02132
jeonhaelee <jhealee0624@gmail.com>
2022-08-18 01:53:55 +0900

    올바른 괄호_nonfin

--- prac74.py
+++ prac74.py
@@ -3,7 +3,29 @@
 def solution(s):
     answer = True
 
-    return True
+    head = 0; tail = 0
+    for w in s:
+        if w == "(":
+            head += 1
+        elif w == ")":
+            tail += 1
+        
+        if head < tail:
+            return False
+        
+    if head != tail:
+        return False
+    else:
+        return True
 
 s = "()()"
 print(solution(s)) # True
+
+s = "(())()"
+print(solution(s)) # True
+
+s = ")()("
+print(solution(s)) # False
+
+s = "(()("
+print(solution(s)) # False
