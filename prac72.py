eb1b6eb7b35d9c160951e0b9c818e6693a5b01d2
jeonhaelee <jhealee0624@gmail.com>
2022-08-20 18:02:32 +0900

    조이스틱_nonfin

--- prac72.py
+++ prac72.py
@@ -3,9 +3,18 @@
 def solution(name):
     answer = 0
     
-    for n in name:
-        answer += min(ord(n)-ord('A'), ord('Z')-ord(n))
-    print(ord(name[0]))
+    for i, n in enumerate(name):
+        answer += min(ord(n)-ord('A'), ord('Z')-ord(n)+1)
+
+    min_move = len(name) - 1
+    next = i + 1
+    while next < len(name) and name[next] == 'A':
+        
+        next += 1
+        
+        min_move = min([min_move, 2 * i + len(name) - next, i + 2 * (len(name) -next)])
+    answer += min_move
+    
     return answer
 
 name = "JEROEN"
