8a329f2902c7532366f24946f0c9e38daa355767
jeonhaelee <jhealee0624@gmail.com>
2022-08-22 00:09:19 +0900

    멀리 뛰기_nonfin

--- prac76.py
+++ prac76.py
@@ -1,7 +1,27 @@
-# 멀리 뛰기
+# 멀리 뛰기 - 1칸 또는 2칸씩
+
+from itertools import permutations
 
 def solution(n):
     answer = 0
+    
+    if n % 2 == 0:
+        two_num = n // 2
+        for two_count in range(two_num):
+            num_li = [1] * (n - two_count)
+            two_li = [2] * two_count
+            num_li.extend(two_li)
+            result = list(permutations(num_li))
+            answer += len(result)
+            
+    else:
+        two_num = n // 2
+        num_li = [1] * (n - two_count + 1)
+        two_li = [2] * two_count
+        num_li.extend(two_li)
+        result = list(permutations(num_li))
+        answer += len(result)
+        
     return answer % 1234567
 
 n = 4
