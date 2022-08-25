c1f595d48bd73a27dd524fabb25107aff86fa79c
jeonhaelee <jhealee0624@gmail.com>
2022-08-25 23:38:07 +0900

    게임 맵 최단거리_nonfin

--- prac77.py
+++ prac77.py
@@ -4,6 +4,12 @@
 
 def solution(maps):
     answer = 0
+    
+    for x in range(5):
+        for y in range(5):
+            print(maps[x][y], end="")
+        print()
+    
     return answer
 
 maps = [[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]]
