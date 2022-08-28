81ea00e89ab37a2e208256512aacc0862f177fab
jeonhaelee <jhealee0624@gmail.com>
2022-08-28 23:58:40 +0900

    게임 맵 최단거리_nonfin

--- prac77.py
+++ prac77.py
@@ -3,20 +3,25 @@
 # (진영에 도착 못 할 시, -1 리턴)
 
 def solution(maps):
-    answer = 0
+    answer_li = []
     
     for x in range(5):
         for y in range(5):
             print(maps[x][y], end="")
         print()
     
+    # while문 거리 리스트 만들기
+    count += 1
     for x in range(5):
         for y in range(5):
             if maps[x][y] == 0:
-                pass
+                continue
+            else:
+                count += 1
+    answer_li.append(count)
                 
         
-    return answer
+    return min(answer_li)
 
 maps = [[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]]
 print(solution(maps)) # 11
