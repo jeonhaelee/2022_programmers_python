e0d826d46d9cbf0a1a44790951a917db8ba20fef
jeonhaelee <jhealee0624@gmail.com>
2022-08-31 23:49:41 +0900

    타겟 넘버_nonfin

--- prac78.py
+++ prac78.py
@@ -5,6 +5,58 @@ def solution(numbers, target):
     answer = 0
     return answer
 
+###
+
+import math
+
+def solution(numbers, target):
+    true_answer = 0
+    li_answer = []
+
+    count = 0
+    while count < len(numbers):
+      answer = numbers[count]
+      li = []
+      for i in range(len(numbers)):
+        if i == count:
+          li.append(['+', numbers[count]])
+          continue
+        if answer < target:
+          answer += numbers[i]
+          li.append(['+', numbers[i]])
+        else :
+          answer -= numbers[i]
+          li.append(['-', numbers[i]])
+      
+      if li not in li_answer:
+        li_answer.append(li)
+        if answer == target:
+          true_answer += 1
+
+
+      answer = -numbers[count]
+      li = []
+      for i in range(len(numbers)):
+        if i == count:
+          li.append(['-', numbers[count]])
+          continue
+        if answer < target:
+          answer += numbers[i]
+          li.append(['+', numbers[i]])
+        else :
+          answer -= numbers[i]
+          li.append(['-', numbers[i]])
+
+      if li not in li_answer:
+        li_answer.append(li)
+        if answer == target:
+          true_answer += 1
+
+      count += 1
+
+
+    return true_answer
+
 numbers = [1, 1, 1, 1, 1]
 target = 3
 print(solution(numbers, target)) # 5
