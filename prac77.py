bb33eb71f6ae78ef8f9678b6016f3a73d481c123
jeonhaelee <jhealee0624@gmail.com>
2022-09-02 22:29:16 +0900

    게임 맵 최단거리_nonfin

--- prac77.py
+++ prac77.py
@@ -6,34 +6,6 @@ def solution(maps):
 
     n = len(maps); m = len(maps[0])
 
-    count = 0
-    x = 0; y = 0
-    while count < 15:
-        if x == n-1 and y == m-1:
-            return count + 1
-        
-        if x+1 < n and y < m:
-            if maps[x+1][y] == 1:
-                maps[x][y] = 0
-                x += 1
-                count += 1
-                continue
-        if x < n and y+1 < m:
-            if maps[x][y+1] == 1:
-                maps[x][y] = 0
-                y += 1
-                count += 1
-                continue
-        if x-1 >= 0 and y < m:
-            if maps[x-1][y] == 1:
-                maps[x][y] = 0
-                x -= 1
-                count += 1
-                continue
-            else:
-                break
-
-    
     return -1
 
 
