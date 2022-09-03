200ce72b1b6cf3a3cead4dcfc33b822b0f11310f
jeonhaelee <jhealee0624@gmail.com>
2022-09-04 00:24:52 +0900

    타겟 넘버_nonfin

--- prac78.py
+++ prac78.py
@@ -1,8 +1,33 @@
 # 타겟 넘버
-# 다시 스스로 해보자.
 
+from itertools import permutations
+
+def get_result(numbers, cal, target):
+    
+    result = numbers[0]
+    for c in cal:
+        if c == 0:
+            continue
+        if result >= target:
+            result -= numbers[c]
+        else:
+            result += numbers[c]
+    
+    return result
+    
+    
+    
 def solution(numbers, target):
     answer = 0
+    l = [i for i in range(0, len(numbers))]
+    cal_li = list(set(permutations(l)))
+    print(cal_li)
+        
+    for cal in cal_li:
+        result = get_result(numbers, cal, target)
+        if result == target:
+            answer += 1
+            
     return answer
 
 numbers = [1, 1, 1, 1, 1]
