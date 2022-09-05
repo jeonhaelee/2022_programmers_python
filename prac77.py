e343f6b3c413071ed5bfbc86f00107b82b2b6f9a
jeonhaelee <jhealee0624@gmail.com>
2022-09-05 23:57:32 +0900

    게임 맵 최단거리_nonfin

--- prac77.py
+++ prac77.py
@@ -2,13 +2,24 @@
 # 검은색 : 벽(0), 흰색 : 길(1)
 # (진영에 도착 못 할 시, -1 리턴)
 
+def draw_map(maps):
+    for i in range(len(maps)):
+        for j in range(len(maps[0])):
+            print(maps[i][j], end = "")
+        print()
+
 def solution(maps):
 
     n = len(maps); m = len(maps[0])
 
+    if maps[n-2][m-1] == 0 and maps[n-1][m-2] == 0:
+        return -1
+    
+    
     x = 0; y = 0
     while x < n and y < m:
         print(f'x : {x}, y : {y}')
+        draw_map(maps)
         
         if x == n-1 and y == m-1:
             return maps[n-1][m-1]
@@ -34,8 +45,8 @@ def solution(maps):
 
 
 
-maps = [[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]]
-print(solution(maps)) # 11
+# maps = [[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]]
+# print(solution(maps)) # 11
 
-# maps = [[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]]
-# print(solution(maps)) # -1
+maps = [[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]]
+print(solution(maps)) # -1
