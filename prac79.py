8607afec114e1ff4a288ca60534b39c56ca60c94
jeonhaelee <jhealee0624@gmail.com>
2022-09-09 23:32:18 +0900

    이진 변환 반복하기_fin

--- prac79.py
+++ prac79.py
@@ -1,7 +1,7 @@
 # 이진 변환 반복하기
-# 이진 변환의 횟수와 변환 과정에서 제거된 모든 0의 개수를 각각 배열에 담아 return
 
 
+# 내 풀이
 import sys
 sys.setrecursionlimit(10**7)
 
@@ -28,3 +28,14 @@ def solution(s):
 
 s = "110010101001"
 print(solution(s)) # [3,8]
+
+
+# 다른 사람 풀이
+def solution(s):
+    a, b = 0, 0
+    while s != '1':
+        a += 1
+        num = s.count('1')
+        b += len(s) - num
+        s = bin(num)[2:]
+    return [a, b]
