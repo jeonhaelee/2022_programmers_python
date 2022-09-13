6238a594ba7ac81b8a812e9c85f55b9db31560d4
jeonhaelee <jhealee0624@gmail.com>
2022-09-13 16:17:02 +0900

    괄호 회전하기_nonfin

--- prac80.py
+++ prac80.py
@@ -1,8 +1,21 @@
 # 괄호 회전하기
 
-
+def check_true(s):
+    result = 0
+    
+    return result
+    
 def solution(s):
-    answer = -1
+    answer = 0
+    count = 0
+    while count < len(s):
+        s = s[1:] + s[0]
+        result = check_true(s)
+        if result:
+            return result
+        else:
+            count += 1
+        
     return answer
 
 s = "[](){}"
