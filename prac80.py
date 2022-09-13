bf8ca896fa3d2ad6f040d4b28a1b3398a3e37ea2
jeonhaelee <jhealee0624@gmail.com>
2022-09-13 16:35:43 +0900

    괄호 회전하기_nonfin

--- prac80.py
+++ prac80.py
@@ -26,9 +26,33 @@ def before_check(s):
 
         
 def check_true(s):
-    result = 0
     
-    return result
+    head1 = 0; tail1 = 0
+    head2 = 0; tail2 = 0
+    head3 = 0; tail3 = 0
+    
+    for i in range(len(s)):
+        if s[i] == "[":
+            head1 += 1
+        elif s[i] == "]":
+            tail1 += 1
+        elif s[i] == "(":
+            head2 += 1
+        elif s[i] == ")":
+            tail2 += 1
+        elif s[i] == "{":
+            head3 += 1
+        elif s[i] == "}":
+            tail3 += 1
+        
+        if head1 < tail1:
+            return False
+        if head2 < tail2:
+            return False
+        if head3 < tail3:
+            return False
+        
+    return True
     
     
 def solution(s):
@@ -40,12 +64,17 @@ def solution(s):
     if not before_check(s):
         return 0
     
+    # 처음 입력받은 s가 올바른 괄호이면 값 리턴.
+    result = check_true(s)
+    if result:
+        return result
+    
+    # s 회전하여 올바른 괄호인지 확인하기.
     count = 0
     while count < len(s):
         s = s[1:] + s[0]
-        result = check_true(s)
-        if result:
-            return result
+        if check_true(s):
+            return count
         else:
             count += 1
         
@@ -54,8 +83,11 @@ def solution(s):
 s = "[](){}"
 print(solution(s)) # 3
 
-s = "}]()[{"
-print(solution(s)) # 2
+# s = "}]()[{"
+# print(solution(s)) # 2
 
-s = "[)(]"
-print(solution(s)) # 0
+# s = "[)(]"
+# print(solution(s)) # 0
+
+# s = "}}}"
+# print(solution(s)) # 0
