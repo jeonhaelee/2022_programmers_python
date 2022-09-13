794d4141cb1f924734716e6e9d12efa0c8ec5227
jeonhaelee <jhealee0624@gmail.com>
2022-09-13 16:24:09 +0900

    괄호 회전하기_nonfin

--- prac80.py
+++ prac80.py
@@ -1,12 +1,45 @@
 # 괄호 회전하기
 
+
+def before_check(s):
+    word_li = [[0, 0], [0, 0], [0, 0]]
+    
+    for w in s:
+        if w == "[":
+            word_li[0][0] += 1
+        elif w == "]":
+            word_li[0][1] += 1
+        elif w == "(":
+            word_li[1][0] += 1
+        elif w == ")":
+            word_li[1][1] += 1
+        elif w == "{":
+            word_li[2][0] += 1
+        elif w == "}":
+            word_li[2][1] += 1
+        
+    for word in word_li:
+        if word[0] != word[1]:
+            return False
+
+    return True
+
+        
 def check_true(s):
     result = 0
     
     return result
     
+    
 def solution(s):
     answer = 0
+    
+    if len(s) % 2 != 0:
+        return 0
+    
+    if not before_check(s):
+        return 0
+    
     count = 0
     while count < len(s):
         s = s[1:] + s[0]
