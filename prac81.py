ed3101c17e4cde94d7b200e9c1f475eb5027f5e4
jeonhaelee <jhealee0624@gmail.com>
2022-09-16 16:27:32 +0900

    주차 요금 계산_nonfin

--- prac81.py
+++ prac81.py
@@ -1,8 +1,38 @@
 # 주차 요금 계산
+# 기본시간, 기본요금, 단위시간, 단위요금
 
+
+from datetime import datetime
 
 def solution(fees, records):
     answer = []
+    car_li = []; time_li = []
+    dic = {}
+    
+    for record in records:
+        
+        car_num = record[6:10]
+        if car_num not in dic:
+            dic[car_num] = 0
+            
+        if record[11:] == "IN": # in 일 때
+            if car_num not in car_li:
+                car_li.append(car_num)
+                d = datetime.strptime(record[:5], '%H:%M')
+                time_li.append(d)
+                
+        else: # out 일 때
+            idx = car_li.index(car_num)
+            d = datetime.strptime(record[:5], '%H:%M') - time_li[idx]
+            dic[car_num] += d
+            del car_li[idx]
+            del time_li[idx]
+            
+            
+    ## 누적시간에 따른 요금계산 함수 만들기
+    ## car_li에 남아있는 아직 out 하지 않은 자동차 시간도 따져서 더해주기
+    
+    
     return answer
 
 
