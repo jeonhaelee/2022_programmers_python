5e9364e72e443fcafc4a8da96852d398f587f07c
jeonhaelee <jhealee0624@gmail.com>
2022-09-19 23:59:59 +0900

    주차 요금 계산_nonfin

--- prac81.py
+++ prac81.py
@@ -26,7 +26,7 @@ def cal_cash(fees, dic):
             continue
         
         result = fees[1]
-        result += ceil((dic[c] - fees[0]) // fees[2]) * fees[3]
+        result += ceil((dic[c] - fees[0]) / fees[2]) * fees[3]
         answer.append(result)
     
     return answer
@@ -81,15 +81,15 @@ def solution(fees, records):
     return answer
 
 
-fees = [180, 5000, 10, 600]
-records = ["05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", 
-           "07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"]
-print(solution(fees, records)) # [14600, 34400, 5000]
+# fees = [180, 5000, 10, 600]
+# records = ["05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", 
+#            "07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"]
+# print(solution(fees, records)) # [14600, 34400, 5000]
 
 fees = [120, 0, 60, 591]
 records = ["16:00 3961 IN","16:00 0202 IN","18:00 3961 OUT","18:00 0202 OUT","23:58 3961 IN"]
 print(solution(fees, records)) # [0, 591]
 
-fees = [1, 461, 1, 10]
-records = ["00:00 1234 IN"]
-print(solution(fees, records)) # [14841]
+# fees = [1, 461, 1, 10]
+# records = ["00:00 1234 IN"]
+# print(solution(fees, records)) # [14841]
