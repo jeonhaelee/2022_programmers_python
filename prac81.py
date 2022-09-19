2618cf2d140a6a42f458d452c584af45c5ed2611
jeonhaelee <jhealee0624@gmail.com>
2022-09-20 00:02:14 +0900

    주차 요금 계산_fin

--- prac81.py
+++ prac81.py
@@ -1,6 +1,7 @@
 # 주차 요금 계산
 # 기본시간, 기본요금, 단위시간, 단위요금
 
+# 내 풀이
 
 from datetime import datetime
 from math import ceil
@@ -51,18 +52,13 @@ def solution(fees, records):
         else: # out 일 때
             idx = car_li.index(car_num)
             d = datetime.strptime(record[:5], '%H:%M') - time_li[idx]
-            print(f'd : {d}')
             dic[car_num] += d
-            print(f'car_num : {car_num}')
-            print(f'dic[car_num] : {dic[car_num]}')
+
             del car_li[idx]
             del time_li[idx]
             
 
     ## car_li에 남아있는 아직 out 하지 않은 자동차 시간도 따져서 더해주기
-    print(f'car_li : {car_li}')
-    print(f'time_li : {time_li}')
-    
     if len(car_li) > 0:
         for i in range(len(car_li)):
             d = datetime.strptime('23:59', '%H:%M') - time_li[i]
@@ -70,11 +66,8 @@ def solution(fees, records):
             
             
     dic = dict(sorted(dic.items()))
-    print(f'dic : {dic}')
     
     cal_time(dic)
-    
-    print(f'dic : {dic}')
     
     answer = cal_cash(fees, dic)
     
@@ -86,10 +79,43 @@ def solution(fees, records):
 #            "07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"]
 # print(solution(fees, records)) # [14600, 34400, 5000]
 
-fees = [120, 0, 60, 591]
-records = ["16:00 3961 IN","16:00 0202 IN","18:00 3961 OUT","18:00 0202 OUT","23:58 3961 IN"]
-print(solution(fees, records)) # [0, 591]
+# fees = [120, 0, 60, 591]
+# records = ["16:00 3961 IN","16:00 0202 IN","18:00 3961 OUT","18:00 0202 OUT","23:58 3961 IN"]
+# print(solution(fees, records)) # [0, 591]
 
 # fees = [1, 461, 1, 10]
 # records = ["00:00 1234 IN"]
 # print(solution(fees, records)) # [14841]
+
+
+# 다른 사람 풀이
+
+from collections import defaultdict
+from math import ceil
+
+class Parking:
+    def __init__(self, fees):
+        self.fees = fees
+        self.in_flag = False
+        self.in_time = 0
+        self.total = 0
+
+    def update(self, t, inout):
+        self.in_flag = True if inout=='IN' else False
+        if self.in_flag:  self.in_time = str2int(t)
+        else:             self.total  += (str2int(t)-self.in_time)
+
+    def calc_fee(self):
+        if self.in_flag: self.update('23:59', 'out')
+        add_t = self.total - self.fees[0]
+        return self.fees[1] + ceil(add_t/self.fees[2]) * self.fees[3] if add_t >= 0 else self.fees[1]
+
+def str2int(string):
+    return int(string[:2])*60 + int(string[3:])
+
+def solution(fees, records):
+    recordsDict = defaultdict(lambda:Parking(fees))
+    for rcd in records:
+        t, car, inout = rcd.split()
+        recordsDict[car].update(t, inout)
+    return [v.calc_fee() for k, v in sorted(recordsDict.items())]
