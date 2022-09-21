131e2c13c9ab09578ea23290cec1c86b1a90592f
jeonhaelee <jhealee0624@gmail.com>
2022-09-22 00:07:22 +0900

    땅따먹기_nonfin

--- prac82.py
+++ prac82.py
@@ -1,5 +1,7 @@
 # 땅따먹기
 # product를 이용해 모든 경우의 수를 고려해보자.
+# 시간 초과 다시
+
 
 from itertools import product
 
@@ -14,17 +16,19 @@ def solution(land):
         lands.append(sub_lands)
             
             
-    print(list(product(*lands)))
+    lands = list(product(*lands))
 
-    # for l in lands:
-    #     result = 0
-    #     for i in range(len(l)-1):
-    #         if l[i] == l[i+1]:
-    #             continue
-    #         result += l[i]
-    #     answer.append(result)
+    for l in lands:
+        result = l[0][1]
+        for i in range(1, len(l)):
+            if l[i-1][0] == l[i][0]:
+                result = 0
+                break
+            result += l[i][1]
+        answer.append(result)
         
     return max(answer)
+    
 
 
 land = [[1,2,3,5],[5,6,7,8],[4,3,2,1]]
