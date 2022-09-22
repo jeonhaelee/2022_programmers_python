55eb404a55472a379bd3975b330113eca3250fb1
jeonhaelee <jhealee0624@gmail.com>
2022-09-22 23:55:02 +0900

    땅따먹기_nonfin

--- prac82.py
+++ prac82.py
@@ -3,32 +3,10 @@
 # 시간 초과 다시
 
 
-from itertools import product
-
 def solution(land):
-    answer = []
-
-    lands = []
-    for l in land:
-        sub_lands = []
-        for i, v in enumerate(l):
-            sub_lands.append((i, v))
-        lands.append(sub_lands)
-            
-            
-    lands = list(product(*lands))
-
-    for l in lands:
-        result = l[0][1]
-        for i in range(1, len(l)):
-            if l[i-1][0] == l[i][0]:
-                result = 0
-                break
-            result += l[i][1]
-        answer.append(result)
+    answer = 0
         
-    return max(answer)
-    
+    return answer
 
 
 land = [[1,2,3,5],[5,6,7,8],[4,3,2,1]]
