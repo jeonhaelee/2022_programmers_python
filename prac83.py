5b5e32c95097cdaff5dc70fee99196082736ec76
jeonhaelee <jhealee0624@gmail.com>
2022-09-23 20:35:16 +0900

    파일명 정리_nonfin

---
+++ prac83.py
@@ -0,0 +1,13 @@
+# 파일명 정리
+
+
+def solution(files):
+    answer = []
+    return answer
+
+
+files = ["img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"]
+print(solution(files))
+
+files = ["F-5 Freedom Fighter", "B-50 Superfortress", "A-10 Thunderbolt II", "F-14 Tomcat"]
+print(solution(files))
