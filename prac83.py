6b5ab5f2fb67c799d3d6ae5ebf503e11f3b4125f
jeonhaelee <jhealee0624@gmail.com>
2022-09-25 23:55:04 +0900

    파일명 정렬_nonfin

--- prac83.py
+++ prac83.py
@@ -1,8 +1,27 @@
-# 파일명 정리
+# 파일명 정렬
+
+
+def check_file(file):
+    head = ""; number = ""; tail = ""
+    for w in file:
+        if w.isdigit():
+            number += w
+        else:
+            if number != "":
+                tail += w
+            else:
+                head += w
+    result = [head, number, tail]
+    return result
 
 
 def solution(files):
     answer = []
+    
+    for file in files:
+        answer.append(check_file(file))
+        
+    print(answer)
     return answer
 
 
