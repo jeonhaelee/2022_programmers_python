23f19099dc3be76f1d746bbfcc2b0064899ea5cb
jeonhaelee <jhealee0624@gmail.com>
2022-10-01 11:46:16 +0900

    큰수만들기2_nonfin

---
+++ prac84.py
@@ -0,0 +1,18 @@
+# 큰수만들기 - 2차
+
+def solution(number, k):
+    answer = []
+   
+    return answer
+
+number = "1924"
+k = 2
+print(solution(number, k)) # "94"
+
+number = "1231234"
+k = 3
+print(solution(number, k)) # "3234"
+
+number = "4177252841"
+k = 4
+print(solution(number, k)) # "775841"
