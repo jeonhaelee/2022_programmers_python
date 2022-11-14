574fbf73f9168a3a9299ee5698e5f9308e1e7c34
jeonhaelee <98138782+jeonhaelee@users.noreply.github.com>
2022-11-14 23:59:54 +0900

    합승 택시 요금_fin +plus

--- prac88.py
+++ prac88.py
@@ -5,19 +5,21 @@ def solution(n, s, a, b, fares):
     answer = 0
     roads = [[1e9]*n for _ in range(n)]
     for i in range(n):
-        roads[i][i] = 0
+        roads[i][i] = 0 # 대각원소는 출발지와 도착지가 같으므로 0 처리
 
-    
+    # 입력받은 fares, roads 행렬에 넣어주기
     for c, d, f in fares:
         roads[c-1][d-1] = f
         roads[d-1][c-1] = f
     
+    # 플로이드 와샬 알고리즘 이용 - 모든 지점에서 지점까지의 최단거리 구해줌
     for k in range(n):
         for i in range(n):
             for j in range(n):
                 if roads[i][j] > roads[i][k] + roads[k][j]:
                     roads[i][j] = roads[i][k] + roads[k][j]
     
+    # 합승할 때의 값들과 비교해 최솟값을 구해줌
     answer = 1e9
     for k in range(n):
         answer = min(answer, roads[s-1][k-1] + roads[k-1][a-1] + roads[k-1][b-1])
